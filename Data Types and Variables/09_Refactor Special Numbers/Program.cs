b4b0dd3087a26977f1aa32caf03ffaf219d28a5d
akkirilov <akkirilov@mail.bg>
2017-01-19 11:46:11 +0200

    Added: 07_Greeting; 08_Refactor Volume of Pyramid; 09_Refactor Special Numbers.

---
+++ Data Types and Variables/09_Refactor Special Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = int.Parse(Console.ReadLine());
+
+            for (int firstNumber = 1; firstNumber <= lastNumber; firstNumber++)
+            {
+                int sum = 0;
+                int checkNumber = firstNumber;
+
+                while (checkNumber > 0)
+                {
+                    sum += checkNumber % 10;
+                    checkNumber = checkNumber / 10;
+                }
+
+                bool isSpecial = false;
+                isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine($"{firstNumber} -> {isSpecial}");
+            }
+        }
+    }
+}

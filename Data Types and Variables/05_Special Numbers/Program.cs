75d4ce88d3ca18e903d31a83df7b9e342274a542
akkirilov <akkirilov@mail.bg>
2017-01-19 11:04:46 +0200

    Added Special Numbers

---
+++ Data Types and Variables/05_Special Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int temp = i;
+                int result = 0;
+
+                while (temp > 0)
+                {
+                    result += (temp % 10);
+                    temp /= 10; 
+                }
+
+                if (result == 5 || result == 7 || result == 11)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+
+                else
+                {
+                    Console.WriteLine("{0} -> False", i);
+                }
+            }
+        }
+    }
+}

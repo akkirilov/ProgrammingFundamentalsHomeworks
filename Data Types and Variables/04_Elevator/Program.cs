82f32e71aae5543323f9d21b61254cb0a4c0ce11
akkirilov <akkirilov@mail.bg>
2017-01-19 10:33:45 +0200

    Added 04_Elevator

---
+++ Data Types and Variables/04_Elevator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int fullcourses = persons / capacity;
+
+            if (persons % capacity == 0)
+            {
+                Console.WriteLine("{0}", (persons / fullcourses));
+            }
+            else
+            {
+                int lastcourse = persons % capacity;
+                Console.WriteLine("{0}", (fullcourses + 1));
+            }
+        }
+    }
+}

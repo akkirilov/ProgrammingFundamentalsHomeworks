5d439990d2e873ebdc3f26fb8726348375262465
akkirilov <akkirilov@mail.bg>
2017-01-19 10:19:37 +0200

    Added 03_Exact Sum of Real Numbers

---
+++ Data Types and Variables/03_Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal result = 0m;
+
+            for (int i = 0; i < n; i++)
+            {
+                result+= decimal.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

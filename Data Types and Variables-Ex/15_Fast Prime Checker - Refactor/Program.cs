4ce14bcba1f22f68b12641e5824113af873caf19
akkirilov <akkirilov@mail.bg>
2017-01-22 00:53:49 +0200

    Added 15_Fast Prime Checker - Refactor

---
+++ Data Types and Variables-Ex/15_Fast Prime Checker - Refactor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Fast_Prime_Checker___Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endNumber = int.Parse(Console.ReadLine());
+            for (int numberToCheck = 2; numberToCheck <= endNumber; numberToCheck++)
+            {
+                bool isPrime = true;
+                for (int divisor = 2; divisor <= Math.Sqrt(numberToCheck); divisor++)
+                {
+                    if (numberToCheck % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{numberToCheck} -> {isPrime}");
+            }
+        }
+    }
+}

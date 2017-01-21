3df4014db4211c3b6fc6f57681b3a09d31fc68b2
akkirilov <akkirilov@mail.bg>
2017-01-22 00:43:43 +0200

    Add exercises 13 and 14

---
+++ Data Types and Variables-Ex/13_Vowel or Digit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Vowel_or_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            string vowels = "aeiou";
+            if (char.IsDigit(input))
+            {
+                Console.WriteLine("digit");
+            }
+            else if (vowels.Contains(input))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}

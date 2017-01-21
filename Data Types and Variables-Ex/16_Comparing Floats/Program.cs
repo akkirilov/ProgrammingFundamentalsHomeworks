1f082d75616a5b2fbd8774f8228706133fc0cd7d
akkirilov <akkirilov@mail.bg>
2017-01-22 01:04:32 +0200

    Added 16_Comparing Floats

---
+++ Data Types and Variables-Ex/16_Comparing Floats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Comparing_Floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+
+            double max = Math.Max(a, b);
+            double min = Math.Min(a, b);
+
+            if (max - min <= eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}

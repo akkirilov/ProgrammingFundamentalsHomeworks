86ae0c26985ec1ea41555742f69a4a902ce0c243
akkirilov <akkirilov@mail.bg>
2017-01-22 21:22:34 +0200

    code formation

--- Data Types and Variables-Ex/19_Thea the Photographer/Program.cs
+++ Data Types and Variables-Ex/19_Thea the Photographer/Program.cs
@@ -10,25 +10,25 @@ namespace _19_Thea_the_Photographer
     {
         static void Main(string[] args)
         {
-            double picturesAmount = double.Parse(Console.ReadLine());
+            long picturesAmount = long.Parse(Console.ReadLine());
             double filterTime = double.Parse(Console.ReadLine());
             double filterFactor = double.Parse(Console.ReadLine());
             double uploadTime = double.Parse(Console.ReadLine());
 
-            double time = (picturesAmount * filterTime) + ((Math.Ceiling(picturesAmount * ((filterFactor) / 100))) * uploadTime);
-            
-            int days, hours, minutes, seconds = 0;
+            long time = (long)(((long)(picturesAmount * filterTime) +(long)((long)(Math.Ceiling(picturesAmount * (filterFactor / 100))) * uploadTime)));
 
+            long days = 0;
+            int hours, minutes, seconds = 0;
 
-            days = (int)(time / (60*60*24));
-            time -= (days * (60 * 60 * 24));
+            days = (long)(time / (60 * 60 * 24));
+            time -= (long)(days * (60 * 60 * 24));
             hours = (int)(time / (60 * 60));
-            time -= (hours * (60*60));
+            time -= (hours * (60 * 60));
             minutes =(int)(time / 60);
             time -= (minutes * 60);
             seconds = (int)time;
-            Console.WriteLine($"{days:D2}:{hours:D2}:{minutes:D2}:{seconds:D2}");
 
+            Console.WriteLine($"{days}:{hours:D2}:{minutes:D2}:{seconds:D2}");
         }
     }
 }

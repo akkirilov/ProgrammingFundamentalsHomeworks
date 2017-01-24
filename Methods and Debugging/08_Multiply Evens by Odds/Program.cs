201c8b3b8eff4922c0db6f24499af834a477b4fb
akkirilov <akkirilov@mail.bg>
2017-01-24 13:04:33 +0200

    Added 08_Multiply Evens by Odds

---
+++ Methods and Debugging/08_Multiply Evens by Odds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = MultiplyEvensByOdds(number);
+            Console.WriteLine(result);
+        }
+
+        private static int MultiplyEvensByOdds(int number)
+        {
+            return SumEvensOrOdds(number, 1) * SumEvensOrOdds(number , 0);
+        }
+        private static int SumEvensOrOdds(int number, int reminder)
+        {
+            int result = 0;
+            foreach (char item in number.ToString())
+            {
+                if ((item - '0') % 2 == reminder)
+                {
+                    result += (item - '0');
+                }
+            }
+            return result;
+        }
+    }
+}

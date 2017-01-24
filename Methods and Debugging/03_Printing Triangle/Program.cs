d8c30ee5e7862a974e374a148afb5dce3d9eece0
akkirilov <akkirilov@mail.bg>
2017-01-24 11:51:31 +0200

    Added 03_Printing Triangle

---
+++ Methods and Debugging/03_Printing Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        private static void PrintTriangle(int n)
+        {
+            PrintHalfTriangle(n, 0);
+            PrintHalfTriangle(n, n);
+        }
+
+        private static void PrintHalfTriangle(int n, int a)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= Math.Abs(a - row); col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

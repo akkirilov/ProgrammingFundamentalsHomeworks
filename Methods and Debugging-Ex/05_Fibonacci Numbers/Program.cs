6be21d90f759a9407a5c7ecce05cbb9fe3868045
akkirilov <akkirilov@mail.bg>
2017-01-24 22:06:49 +0200

    Added 05_Fibonacci Numbers

---
+++ Methods and Debugging-Ex/05_Fibonacci Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNFib(n);
+        }
+
+        private static void PrintNFib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                long a = 1;
+                long b = 1;
+                long c = 2;
+                while (n - 1 > 0)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                    n--;
+                }
+                Console.WriteLine(c);
+            }
+        }
+    }
+}

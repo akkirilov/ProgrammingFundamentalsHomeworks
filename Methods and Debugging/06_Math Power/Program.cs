e615cf9c45a3948d6bcfd8ea919ab82d46097d04
akkirilov <akkirilov@mail.bg>
2017-01-24 12:24:53 +0200

    Added 06_Math Power

---
+++ Methods and Debugging/06_Math Power/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+
+            double result = GetPowedNumber(baseNumber, pow);
+
+            Console.WriteLine(result);
+        }
+
+        private static double GetPowedNumber(double baseNumber, int pow)
+        {
+            double result = 1;
+
+            for (int i = 0; i < pow; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+    }
+}

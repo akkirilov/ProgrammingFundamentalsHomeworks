5a1dcf20d6039ee0a076bb096a5483ca21740e22
akkirilov <akkirilov@mail.bg>
2017-01-24 12:13:20 +0200

    Added 04_Draw a Filled Square

---
+++ Methods and Debugging/04_Draw a Filled Square/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Draw_a_Filled_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSquare(n);
+        }
+
+        private static void PrintSquare(int n)
+        {
+            PrintStartEnd(n);
+            PrintBoddy(n);
+            PrintStartEnd(n);
+        }
+
+        private static void PrintBoddy(int n)
+        {
+            for (int i = 1; i < n-1; i++)
+            {
+                Console.Write('-');
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write("\\/", n);
+                }
+                Console.WriteLine('-');
+            }
+        }
+
+        private static void PrintStartEnd(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+    }
+}

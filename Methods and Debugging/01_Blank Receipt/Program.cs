4c0de0c5a75f0d4f173c8710817e7f5e9b18b71e
akkirilov <akkirilov@mail.bg>
2017-01-24 11:06:33 +0200

    Added 2 solutions

---
+++ Methods and Debugging/01_Blank Receipt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Blank_Receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+
+        private static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintBottom();
+        }
+
+        private static void PrintBottom()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+
+        private static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        private static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+    }
+}

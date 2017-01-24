8dfe9cbbfd42569bd4ed51def2fc8bad6e10accf
akkirilov <akkirilov@mail.bg>
2017-01-24 22:39:41 +0200

    Added 06_Prime Checker

---
+++ Methods and Debugging-Ex/06_Prime Checker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            bool isPrimeNumber = IsPrime(n);
+
+            Console.WriteLine(isPrimeNumber);
+        }
+
+        private static bool IsPrime(long n)
+        {
+            if (n <= 1 || n % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 3; i <= (int)Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}

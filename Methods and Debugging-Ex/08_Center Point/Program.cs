79c0b648d669e44c9542290f8c0d082425f3a102
akkirilov <akkirilov@mail.bg>
2017-01-25 11:03:30 +0200

    Added 08_Center Point

---
+++ Methods and Debugging-Ex/08_Center Point/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double distanceXY1 = GetDistanceFromZero(x1, y1);
+            double distanceXY2 = GetDistanceFromZero(x2, y2);
+
+            if (distanceXY1 < distanceXY2)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+
+        private static double GetDistanceFromZero(double x, double y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+    }
+}

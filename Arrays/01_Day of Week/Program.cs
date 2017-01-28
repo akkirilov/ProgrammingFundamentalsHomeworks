1886e442dd176a41d490f3236369f949d3fb9968
akkirilov <akkirilov@mail.bg>
2017-01-28 19:30:37 +0200

    Added 01_Day of Week and 02_Reverse an Array of Integers

---
+++ Arrays/01_Day of Week/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = new string[] { "Invalid Day!", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int n = int.Parse(Console.ReadLine());
+            if (n > 7 || n < 0)
+            {
+                Console.WriteLine(daysOfWeek[0]);
+            }
+            else
+            {
+                Console.WriteLine(daysOfWeek[n]);
+            }
+        }
+    }
+}
---
+++ Arrays/02_Reverse an Array of Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Reverse_an_Array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arrayOfNumbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arrayOfNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = arrayOfNumbers.Length - 1; i >= 0 ; i--)
+            {
+                Console.Write(arrayOfNumbers[i]);
+                if (i != 0)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}

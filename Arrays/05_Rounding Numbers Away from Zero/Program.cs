a8c27922faf5ac9820c4abfdec6029397aab4fc2
akkirilov <akkirilov@mail.bg>
2017-01-28 19:31:59 +0200

    Added 05_Rounding Numbers Away from Zero.csproj

---
+++ Arrays/05_Rounding Numbers Away from Zero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Rounding_Numbers_Away_from_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double afterRound = Math.Abs(array[i]);
+                if (afterRound >= (int)afterRound + 0.5)
+                {
+                    afterRound = (int)afterRound + 1;
+                }
+                else
+                {
+                    afterRound = (int)afterRound;
+                }
+
+                if (array[i] < 0)
+                {
+                    afterRound *= -1;
+                }
+                
+                Console.WriteLine($"{array[i]} => {(int)afterRound}");
+            }
+        }
+    }
+}

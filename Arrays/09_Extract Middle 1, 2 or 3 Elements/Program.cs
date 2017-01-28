5a5982de932cd8fdc94a4e1b2ce9b63c473d7097
akkirilov <akkirilov@mail.bg>
2017-01-28 19:34:25 +0200

    Added 08_Condense Array to Number AND 09_Extract Middle 1, 2 or 3 Elements

---
+++ Arrays/09_Extract Middle 1, 2 or 3 Elements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Extract_Middle_1__2_or_3_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (nums.Length == 1)
+            {
+                Console.WriteLine("{ " + nums[0] + " }");
+                return;
+            }
+            
+            switch (nums.Length % 2)
+            {
+                case 1:
+                    Console.WriteLine("{ " + nums[nums.Length / 2 - 1] + ", " 
+                        + nums[nums.Length / 2] + ", " + nums[nums.Length / 2 + 1] + " }");
+                    break;
+                case 0:
+                    Console.WriteLine("{ " + nums[nums.Length / 2 - 1] 
+                        + ", " + nums[nums.Length / 2] + " }");
+                    break;
+            }
+
+            if (nums.Length == 1)
+            {
+                Console.WriteLine("{ " + nums[0] + " }");
+            }
+        }
+    }
+}

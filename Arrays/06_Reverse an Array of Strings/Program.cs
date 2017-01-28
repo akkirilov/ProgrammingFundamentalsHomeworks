f381b1bcfe4d0f85a023a2d961a1bbb8415ee623
akkirilov <akkirilov@mail.bg>
2017-01-28 19:33:20 +0200

    Added 06_Reverse an Array of Strings AND 07_Sum Arrays

---
+++ Arrays/06_Reverse an Array of Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Reverse_an_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                string temp = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = array[i];
+                array[i] = temp;
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
---
+++ Arrays/07_Sum Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Sum_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] result = new int[Math.Max(arr1.Length, arr2.Length)];
+            
+            for (int i = 0; i < result.Length; i++)
+            {
+                int a = arr1[i % arr1.Length];
+                int b = arr2[i % arr2.Length];
+                result[i] = a + b;
+            }
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}

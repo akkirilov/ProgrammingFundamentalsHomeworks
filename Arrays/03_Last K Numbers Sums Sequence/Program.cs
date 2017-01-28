ecf26af64bcbf598a0f55feb292f3578b416fa28
akkirilov <akkirilov@mail.bg>
2017-01-28 19:31:16 +0200

    Added 03_Last K Numbers Sums Sequence AND 04_Triple Sum

---
+++ Arrays/03_Last K Numbers Sums Sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Last_K_Numbers_Sums_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sequence = int.Parse(Console.ReadLine());
+            long[] arrayOfNumbers = new long[n];
+            arrayOfNumbers[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                int seq = 0;
+                if (sequence < i)
+                {
+                    seq = i - sequence;
+                }
+                for (int j = i; j >= seq; j--)
+                {
+                    arrayOfNumbers[i] += arrayOfNumbers[j];
+                }
+            }
+            Console.WriteLine(String.Join(" ", arrayOfNumbers));
+        }
+    }
+}
---
+++ Arrays/04_Triple Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Triple_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] array = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            bool found = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                long a = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    long b = array[j];
+                    for (int k = 0; k < array.Length; k++)
+                    {
+                        long sum = array[k];
+                        if (sum == a + b)
+                        {
+                            Console.WriteLine($"{a} + {b} == {sum}");
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}

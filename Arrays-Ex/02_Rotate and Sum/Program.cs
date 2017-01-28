044b1b4547dde9efc13cbbcc9810eb8904148e45
akkirilov <akkirilov@mail.bg>
2017-01-28 23:42:05 +0200

    Added 02_Rotate and Sum AND 03_Fold and Sum

---
+++ Arrays-Ex/02_Rotate and Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers1 = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long[] numbers2 = new long[numbers1.Length];
+
+            long[] sum = new long[numbers1.Length];
+
+            int cicles = int.Parse(Console.ReadLine());
+
+            while (cicles > 0)
+            {
+                numbers2[0] = numbers1[numbers1.Length - 1];
+                sum[0] += numbers2[0];
+                for (int i = 1; i < numbers1.Length; i++)
+                {
+                    numbers2[i] = numbers1[i - 1];
+                    sum[i] += numbers2[i];
+                }
+
+                numbers1 = numbers2.ToArray();
+                cicles--;
+            }
+            Console.WriteLine(String.Join(" ", sum));
+        }
+    }
+}
---
+++ Arrays-Ex/03_Fold and Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            int quarter = numbers.Length / 4;
+            long[] sum = new long[quarter * 2];
+
+            int endIndex = numbers.Length - 1;
+            int startSumStart = quarter * 2 - 1;
+            int startSumEnd = 2 * quarter;
+
+            for (int startIndex = 0; startIndex < quarter; startIndex++)
+            {
+                sum[quarter - startIndex - 1] = numbers[startSumStart] + numbers[startIndex];
+                sum[quarter + startIndex] = numbers[startSumEnd] + numbers[endIndex];
+
+                startSumStart--;
+                startSumEnd++;
+                endIndex--;
+            }
+
+            Console.WriteLine(String.Join(" ", sum));
+        }
+    }
+}

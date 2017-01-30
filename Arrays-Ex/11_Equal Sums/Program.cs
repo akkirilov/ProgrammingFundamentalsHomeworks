cc9516fc67b3473e99594fb5d100b2f4d17528fe
akkirilov <akkirilov@mail.bg>
2017-01-30 16:18:54 +0200

    Added 11_Equal Sums

---
+++ Arrays-Ex/11_Equal Sums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Equal_Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int separator;
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                separator = i;
+
+                for (int j = 0; j < separator; j++)
+                {
+                    sumLeft += array[j];
+                }
+
+                for (int j = separator + 1; j < array.Length; j++)
+                {
+                    sumRight+= array[j];
+                }
+
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine(separator);
+                    return;
+                }
+
+                sumLeft = 0;
+                sumRight = 0;
+            }
+            Console.WriteLine("no");
+        }
+    }
+}

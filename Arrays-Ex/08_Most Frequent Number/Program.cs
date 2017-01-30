e7682495cb360e975a5587e0451fb75244dc25a9
akkirilov <akkirilov@mail.bg>
2017-01-30 15:06:50 +0200

    Added 08_Most Frequent Number

---
+++ Arrays-Ex/08_Most Frequent Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 0;
+            int bestCount = 0;
+            int mosFrequentNumber = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > bestCount)
+                {
+                    bestCount = count;
+                    mosFrequentNumber = array[i];
+                }
+                    count = 0;
+            }
+            Console.WriteLine(mosFrequentNumber);
+        }
+    }
+}

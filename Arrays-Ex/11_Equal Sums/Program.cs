1ad55338d5e73db4632a4a8c71ed5f1dd63709db
akkirilov <akkirilov@mail.bg>
2017-01-30 17:25:07 +0200

    Code formation

--- Arrays-Ex/11_Equal Sums/Program.cs
+++ Arrays-Ex/11_Equal Sums/Program.cs
@@ -12,41 +12,29 @@ namespace _11_Equal_Sums
         {
             int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int separator;
-            int sumLeft = 0;
-            int sumRight = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 separator = i;
 
-                for (int j = 0; j < separator; j++)
-                {
-                    sumLeft += array[j];
-                }
-
-                for (int j = separator + 1; j < array.Length; j++)
-                {
-                    sumRight+= array[j];
-                }
-
-                if (SumPartOfArray(array, 0, separator) == sumRight)
+                if (SumPartOfArray(array, 0, separator) == SumPartOfArray(array, separator + 1, array.Length))
                 {
                     Console.WriteLine(separator);
                     return;
                 }
-
-                sumLeft = 0;
-                sumRight = 0;
             }
             Console.WriteLine("no");
         }
 
         private static int SumPartOfArray(int[] array, int startIndex, int endIndex)
         {
+            int sum = 0;
             for (int i = startIndex; i < endIndex; i++)
             {
-                sumLeft += array[i];
+                sum += array[i];
             }
+
+            return sum;
         }
     }
 }

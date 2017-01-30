45571f10940034db6c0f794bb7bf5d9b2c1e5981
akkirilov <akkirilov@mail.bg>
2017-01-30 16:08:56 +0200

    Added 09_Index of Letters AND 10_Pairs by Difference

---
+++ Arrays-Ex/09_Index of Letters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = input[i] - 'a';
+                Console.WriteLine(input[i] + " -> " + index);
+            }
+        }
+    }
+}

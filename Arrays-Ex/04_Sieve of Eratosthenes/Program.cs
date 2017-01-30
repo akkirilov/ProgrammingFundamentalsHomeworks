b708217484afb85a455b8196c1f076ff17a66d73
akkirilov <akkirilov@mail.bg>
2017-01-30 12:15:27 +0200

    Added 04_Sieve of Eratosthenes

---
+++ Arrays-Ex/04_Sieve of Eratosthenes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Sieve_of_Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            bool[] isPrimes = new bool[n + 1];
+            for (int i = 2; i < isPrimes.Length; i++)
+            {
+                isPrimes[i] = true;
+            }
+
+            for (int m = 2; m < isPrimes.Length; m++)
+            {
+                if (isPrimes[m] == true)
+                {
+                    for (int i = m; i < isPrimes.Length; i++)
+                    {
+                        if (i + m < isPrimes.Length)
+                        {
+                            isPrimes[i + m] = false;
+                            i += m - 1;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < isPrimes.Length; i++)
+            {
+                if (isPrimes[i] == true)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}

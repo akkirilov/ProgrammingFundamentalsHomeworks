c4a82d52d424b420ed0eafb8956eff2700c3c58f
akkirilov <akkirilov@mail.bg>
2017-01-31 14:09:25 +0200

    Added 04_Split by Word Casing

---
+++ Lists/04_Split by Word Casing/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Split_by_Word_Casing
+{
+    class Program
+    {
+        private static int length;
+
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(new char[] { ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<string> upper = new List<string>();
+            List<string> lower = new List<string>();
+            List<string> mixed = new List<string>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                bool isLower = false;
+                bool isUpper = false;
+                bool isMixed = false;
+                for (int w = 0; w < words[i].Length; w++)
+                {
+                    if ((words[i][w] >= '0' && words[i][w] <= '9') || (words[i][w] >= '!' && words[i][w] <= '@'))
+                    {
+                        isLower = false;
+                        isUpper = false;
+                        isMixed = true;
+                        break;
+                    }
+                    else if (words[i][w] >= 'a' && words[i][w] <= 'z')
+                    {
+                        if (isUpper)
+                        {
+                            isLower = false;
+                            isUpper = false;
+                            isMixed = true;
+                            break;
+                        }
+                        isLower = true;
+                    }
+                    else if (words[i][w] >= 'A' && words[i][w] <= 'Z')
+                    {
+                        if (isLower)
+                        {
+                            isLower = false;
+                            isUpper = false;
+                            isMixed = true;
+                            break;
+                        }
+                        isUpper = true;
+                    }
+                }
+
+                if (isMixed)
+                {
+                    mixed.Add(words[i]);
+                }
+                else if(isLower)
+                {
+                    lower.Add(words[i]);
+                }
+                else if (isUpper)
+                {
+                    upper.Add(words[i]);
+                }
+            }
+            
+            Console.WriteLine("Lower-case: {0}", String.Join(", ", lower));
+            Console.WriteLine("Mixed-case: {0}", String.Join(", ", mixed));
+            Console.WriteLine("Upper-case: {0}", String.Join(", ", upper));
+        }
+    }
+}

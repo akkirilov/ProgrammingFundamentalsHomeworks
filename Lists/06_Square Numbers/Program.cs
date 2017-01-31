78da50375c53deaed39b6dd445deb8c353112892
akkirilov <akkirilov@mail.bg>
2017-01-31 14:27:05 +0200

    Added 06_Square Numbers

---
+++ Lists/06_Square Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Square_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).OrderByDescending(x => x).ToList();
+            List<int> squareNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (Math.Sqrt(numbers[i]) % 1 == 0)
+                {
+                    squareNumbers.Add(numbers[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", squareNumbers));
+        }
+    }
+}

73344fcc5deff1d124d08c95b8ef5f2ca6653bac
akkirilov <akkirilov@mail.bg>
2017-01-31 12:26:43 +0200

    Added 02_Append Lists (Linq solution)

---
+++ Lists/02_Append Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Append_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> numbers = Console.ReadLine().Split('|').Select(x => x.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(y => int.Parse(y)).ToList()).Reverse().ToList();
+
+            numbers.ForEach(x => x.ForEach(y => Console.Write(y + " ")));
+        }
+    }
+}

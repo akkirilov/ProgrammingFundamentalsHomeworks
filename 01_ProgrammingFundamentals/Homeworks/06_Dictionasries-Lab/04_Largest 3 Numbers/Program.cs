fa914c93f53667973b15cfc7962f4788877cbbb1
akkirilov <akkirilov@mail.bg>
2017-02-02 17:06:54 +0200

    Added 04_Largest 3 Numbers AND 05_Short Words Sorted

---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/04_Largest 3 Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+
+            numbers = numbers.OrderByDescending(x => x).Take(3).ToList();
+
+            numbers.ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/05_Short Words Sorted/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Short_Words_Sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' };
+
+            List<string> text = Console.ReadLine()
+                .ToLower()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length < 5)
+                .ToList();
+
+            List<string> words = new List<string>();
+
+            for (int i = 0; i < text.Count; i++)
+            {
+                if (!words.Contains(text[i]))
+                {
+                    words.Add(text[i]);
+                }
+            }
+
+            words = words.OrderBy(x => x).ToList();
+
+            Console.WriteLine(String.Join(", ", words));
+        }
+    }
+}

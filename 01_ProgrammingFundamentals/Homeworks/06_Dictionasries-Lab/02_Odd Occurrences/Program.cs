2fde00ef6235fa8297c76a319144a7817cc83c46
akkirilov <akkirilov@mail.bg>
2017-02-02 14:08:09 +0200

    Added 03_Sum, Min, Max, Average

--- 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/02_Odd Occurrences/Program.cs
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/02_Odd Occurrences/Program.cs
@@ -10,6 +10,30 @@ namespace _02_Odd_Occurrences
     {
         static void Main(string[] args)
         {
+            List<string> wordsText = Console.ReadLine().ToLower().Split().ToList();
+
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+            for (int i = 0; i < wordsText.Count; i++)
+            {
+                if (wordsCount.ContainsKey(wordsText[i]))
+                {
+                    wordsCount[wordsText[i]]++;
+                    continue;
+                }
+
+                wordsCount[wordsText[i]] = 1;
+            }
+
+            wordsText.Clear();
+            foreach (var word in wordsCount)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    wordsText.Add(word.Key);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", wordsText));
         }
     }
 }
---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/03_Sum, Min, Max, Average/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Sum__Min__Max__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> sequence = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                sequence.Add(int.Parse(Console.ReadLine()));
+            }
+
+            PrintSum(sequence);
+            PrintMin(sequence);
+            PrintMax(sequence);
+            PrintAverage(sequence);
+        }
+
+        private static void PrintSum(List<int> sequence)
+        {
+            Console.WriteLine("Sum = {0}", sequence.Sum());
+        }
+
+        private static void PrintMin(List<int> sequence)
+        {
+            Console.WriteLine("Min = {0}", sequence.Min());
+        }
+
+        private static void PrintMax(List<int> sequence)
+        {
+            Console.WriteLine("Max = {0}", sequence.Max());
+        }
+
+        private static void PrintAverage(List<int> sequence)
+        {
+            Console.WriteLine("Average = {0}", sequence.Average());
+        }
+    }
+}

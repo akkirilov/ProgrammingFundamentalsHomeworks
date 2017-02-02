512cee2971cbd30fb0a61217452f70fd5ed2148c
akkirilov <akkirilov@mail.bg>
2017-02-02 19:54:34 +0200

    Added 04_Fix Emails

---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionaries-Exercises/04_Fix Emails/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Fix_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> mailBook = new Dictionary<string, string>();
+
+            string name = Console.ReadLine();
+            if (name == "stop")
+            {
+                return;
+            }
+
+            string mail = Console.ReadLine();
+            string domain = mail.Substring(mail.Length - 2).ToLower();
+
+            while (true)
+            {
+                
+                if (!mailBook.ContainsKey(name) && (domain != "uk" || domain != "us"))
+                {
+                    mailBook.Add(name, mail);
+                }
+                else
+                {
+                    mailBook[name] = mail;
+                }
+
+                name = Console.ReadLine();
+                if (name == "stop")
+                {
+                    break;
+                }
+
+                mail = Console.ReadLine().ToLower();
+            }
+
+            PrintMailBook(mailBook);
+        }
+
+        private static void PrintMailBook(Dictionary<string, string> mailBook)
+        {
+            foreach (var contact in mailBook)
+            {
+                Console.WriteLine($"{contact.Key} -> {contact.Value}");
+            }
+        }
+    }
+}

6d1cc6dc1dcb9cc8274cb3deada4b0b36c3a6548
akkirilov <akkirilov@mail.bg>
2017-02-02 19:04:54 +0200

    Added 03_A Miner Task

---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionaries-Exercises/03_A Miner Task/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, decimal> minerBook = new Dictionary<string, decimal>();
+
+            string resource = Console.ReadLine();
+            if (resource == "stop")
+            {
+                return;
+            }
+
+            string quantity = Console.ReadLine();
+
+            while (true)
+            {
+                if (minerBook.ContainsKey(resource))
+                {
+                    minerBook[resource] += decimal.Parse(quantity);
+                }
+                else
+                {
+                    minerBook.Add(resource, decimal.Parse(quantity));
+                }
+
+                resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                quantity = Console.ReadLine();
+            }
+
+            PrintMinerBook(minerBook);
+        }
+
+        private static void PrintMinerBook(Dictionary<string, decimal> minerBook)
+        {
+            foreach (var item in minerBook)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}

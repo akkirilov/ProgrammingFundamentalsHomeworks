24f83b11358c746a89f93c26843f1b15b29a7f9b
akkirilov <akkirilov@mail.bg>
2017-02-02 12:56:53 +0200

    Rename Dir

---
+++ 01_ProgrammingFundamentals/Homeworks/06_Dictionasries-Lab/01_Count Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Count_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+            Dictionary<decimal, int> countNumbers = new Dictionary<decimal, int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (countNumbers.ContainsKey(numbers[i]))
+                {
+                    countNumbers[numbers[i]]++;
+                    continue;
+                }
+                
+                countNumbers.Add(numbers[i], 1);
+            }
+
+            foreach (var number in countNumbers.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}

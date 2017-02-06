a107ee3ac53b12e54a5bc202b2817cf1d13e4147
akkirilov <akkirilov@mail.bg>
2017-02-06 16:58:17 +0200

    not completed: 20160612_04_Trifon’s Quest

---
+++ 01_ProgrammingFundamentals/Exams/20160612_04_Trifon’s Quest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20160612_04_Trifon_s_Quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = int.Parse(Console.ReadLine());
+            int[] coordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[,] map = new int[coordinates[0], coordinates[1]];
+            int col = 0;
+            int turn = 1;
+            while (true)
+            {
+                char[] input = Console.ReadLine().ToUpper().ToCharArray();
+                if (col % 2 == 0)
+                {
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        switch (input[i])
+                        {
+                            case 'F':
+                                health /= turn;
+                                turn++;
+                                break;
+                            case 'H':
+                                health += (health / turn);
+                                turn++;
+                                break;
+                                case 'E':
+                                turn++;
+                                break;
+                            case 'T':
+                                if (input.Length - i - 1 >=2 )
+                                {
+                                    i += 2;
+                                    turn += 2;
+                                }
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = input.Length - 1; i >= 0; i--)
+                    {
+                        switch (input[i])
+                        {
+                            case 'F':
+                                break;
+                            case 'H':
+                                break;
+                            case 'T':
+                                break;
+                            case 'E':
+                                break;
+                        }
+                    }
+                }
+                
+
+                col++;
+            }
+
+        }
+    }
+}

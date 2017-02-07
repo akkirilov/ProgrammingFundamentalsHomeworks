23a788398bc0266842ea292c067aceb2f8106138
akkirilov <akkirilov@mail.bg>
2017-02-07 14:15:01 +0200

    Added 20160911_01_Thea the Photographer

---
+++ 01_ProgrammingFundamentals/Exams/20160911_01_Thea the Photographer/TheaThePhotographer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _20160911_01_Thea_the_Photographer
+{
+    class TheaThePhotographer
+    {
+        static void Main()
+        {
+            long amountPictures = long.Parse(Console.ReadLine());
+            long filterTime = long.Parse(Console.ReadLine()) * amountPictures;
+            long filterFactor = (long)Math.Ceiling((decimal.Parse(Console.ReadLine()) * amountPictures) / 100m);
+            long uploadTime = long.Parse(Console.ReadLine()) * filterFactor;
+
+            long seconds = (long)(filterTime + uploadTime);
+            TimeSpan totalTime = new TimeSpan(seconds * 10000000);
+            Console.WriteLine("{0:d\\:hh\\:mm\\:ss}", totalTime);
+        }
+    }
+}

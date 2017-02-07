f0cadebd100583bceb2f19704205b1f8a08a88fd
akkirilov <akkirilov@mail.bg>
2017-02-07 12:07:24 +0200

    Added 06_Rectangle Position

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/06_Rectangle Position/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Rectangle_Position
+{
+    public class Rectangle
+    {
+        public double left { get; set; }
+        public double top { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+
+        public double bottom => top - height;
+        public double right => bottom + width;
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/06_Rectangle Position/RectanglePosition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Rectangle_Position
+{
+    class RectanglePosition
+    {
+        static void Main()
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Rectangle firstrectangle = new Rectangle { left = input[0], top = input[1], width = input[2], height = input[3] };
+
+            input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Rectangle secondrectangle = new Rectangle { left = input[0], top = input[1], width = input[2], height = input[3] };
+
+            IsFirstInsideSecondRectangle(firstrectangle, secondrectangle);
+        }
+
+        private static void IsFirstInsideSecondRectangle(Rectangle firstrectangle, Rectangle secondrectangle)
+        {
+            if (firstrectangle.top <= secondrectangle.top 
+                && firstrectangle.left >= secondrectangle.left 
+                && secondrectangle.bottom <= firstrectangle.bottom 
+                && secondrectangle.right >= firstrectangle.right )
+            {
+                Console.WriteLine("Inside");
+                return;
+            }
+            Console.WriteLine("Not inside");
+        }
+    }
+}

4559b1eee76197afebe64df4666f5c1e1a59ecc8
akkirilov <akkirilov@mail.bg>
2017-02-07 10:49:24 +0200

    Added 01_Day of Week AND 02_Randomize Words

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/01_Day of Week/DayOfWeek.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Day_of_Week
+{
+    class DayOfWeek
+    {
+        static void Main()
+        {
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/02_Randomize Words/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+                SwapElements(words, i, randomIndex);
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static void SwapElements(string[] text, int i, int randomIndex)
+        {
+            string tempWord = text[i];
+            text[i] = text[randomIndex];
+            text[randomIndex] = tempWord;
+        }
+    }
+}

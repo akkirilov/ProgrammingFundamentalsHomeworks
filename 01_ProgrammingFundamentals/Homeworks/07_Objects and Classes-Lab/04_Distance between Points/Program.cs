61bbc589e4bc13fb77efc155616eed7134c33603
akkirilov <akkirilov@mail.bg>
2017-02-07 11:10:32 +0200

    Added 04_Distance between Points

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/04_Distance between Points/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_Distance_between_Points
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Point firstPoint = new Point { X = input[0], Y = input[1] };
+
+            input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Point secondPoint = new Point { X = input[0], Y = input[1] };
+
+            Console.WriteLine("{0:f3}", GetDistanceBetweenPoints(firstPoint, secondPoint));
+        }
+
+        private static double GetDistanceBetweenPoints(Point firstPoint, Point secondPoint)
+        {
+            return Math.Sqrt((Math.Pow((firstPoint.X - secondPoint.X), 2)) + (Math.Pow((firstPoint.Y - secondPoint.Y), 2)));
+        }
+    }
+}

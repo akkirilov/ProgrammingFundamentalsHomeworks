8ba28607ec25645ee7e9c13e4077efe9fe2ab496
akkirilov <akkirilov@mail.bg>
2017-02-07 11:35:21 +0200

    Added 05_Closest Two Points

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/05_Closest Two Points/ClosestTwoPoints.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Closest_Two_Points
+{
+    class ClosestTwoPoints
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Point> points = new List<Point>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double[] coordinates = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+                points.Add(new Point { X = coordinates[0], Y = coordinates[1] });
+            }
+
+            PrintClosestDistance(points);
+        }
+
+        private static void PrintClosestDistance(List<Point> points)
+        {
+            Point firstPoint = new Point();
+            Point secondPoint = new Point();
+            double distanceMin = double.MaxValue;
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    double currentDistance = GetDistance(points[i], points[j]);
+
+                    if (currentDistance < distanceMin)
+                    {
+                        distanceMin = currentDistance;
+                        firstPoint = points[i];
+                        secondPoint = points[j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"{distanceMin:f2}");
+            Console.WriteLine($"({firstPoint.X}, {firstPoint.Y})");
+            Console.WriteLine($"({secondPoint.X}, {secondPoint.Y})");
+        }
+
+        private static double GetDistance(Point firstPoint, Point secondPoint)
+        {
+            return Math.Sqrt((Math.Pow((firstPoint.X - secondPoint.X), 2)) + (Math.Pow((firstPoint.Y - secondPoint.Y), 2)));
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Lab/05_Closest Two Points/Points.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Closest_Two_Points
+{
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+
+
+    }
+}

fe9bdb2df324cc01b8a591f4e18c2440109cf7c3
akkirilov <akkirilov@mail.bg>
2017-02-08 14:02:23 +0200

    Added 04_Average Grades

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/04_Average Grades/AverageGrades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Average_Grades
+{
+    class AverageGrades
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> studentsList = new List<Student>();
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+                string name = input[0];
+                input.RemoveAt(0);
+                studentsList.Add(new Student { Name = name, Grades = input.Select(double.Parse).ToList() });
+            }
+
+            studentsList = studentsList.Where(x => x.AvarageGrade >= 5).OrderBy(x => x.Name).ThenByDescending(x => x.AvarageGrade).ToList();
+
+            for (int i = 0; i < studentsList.Count; i++)
+            {
+                Console.WriteLine($"{studentsList[i].Name} -> {studentsList[i].AvarageGrade:f2}");
+            }
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/04_Average Grades/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Average_Grades
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public List<double> Grades { get; set; }
+
+        public double AvarageGrade => Grades.Sum() / Grades.Count;
+
+
+    }
+}

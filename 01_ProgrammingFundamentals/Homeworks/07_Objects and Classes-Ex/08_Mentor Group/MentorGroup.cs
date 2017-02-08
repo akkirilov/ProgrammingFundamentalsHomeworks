4eb7b7ab3d2e884da991b0662fc1c5ec37d15c9e
akkirilov <akkirilov@mail.bg>
2017-02-08 21:11:52 +0200

    Need corrections!

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/08_Mentor Group/MentorGroup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace _08_Mentor_Group
+{
+    public class MentorGroup
+    {
+        public static void Main()
+        {
+            SortedDictionary<string, Student> students = new SortedDictionary<string, Student>();
+            
+            string input = Console.ReadLine();
+            while (input != "end of dates")
+            {
+                string[] inputEll = input.Split().ToArray();
+                string name = inputEll[0];
+
+                if (inputEll.Length > 1)
+                {
+                    List<DateTime> currentAttend = inputEll[1].Split(',')
+                        .Select(x => DateTime.ParseExact(x, "dd\\/MM\\/yyyy", CultureInfo.InvariantCulture))
+                        .ToList();
+
+                    if (students.ContainsKey(name))
+                    {
+                        students[name].Attendency.AddRange(currentAttend);
+                    }
+                    else
+                    {
+                        Student currentStudent = new Student
+                        {
+                            Name = name,
+                            Attendency = currentAttend,
+                            Comments = new List<string>()
+                        };
+
+                        students[name] = currentStudent;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "end of comments")
+            {
+                string[] inputEll = input.Split('-').ToArray();
+
+                string name = inputEll[0];
+                string comment = inputEll[1];
+
+                if (students.ContainsKey(name))
+                {
+                    students[name].Comments.Add(comment);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Value.Name);
+
+                Console.WriteLine("Comments:");
+                foreach (var comment in student.Value.Comments)
+                {
+                    Console.WriteLine($"- {comment}");
+                }
+
+                Console.WriteLine("Dates attended:");
+                foreach (var attend in student.Value.Attendency.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {attend.ToString("dd\\/MM\\/yyyy")}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/08_Mentor Group/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Mentor_Group
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public List<string> Comments { get; set; }
+        public List<DateTime> Attendency { get; set; }
+    }
+}

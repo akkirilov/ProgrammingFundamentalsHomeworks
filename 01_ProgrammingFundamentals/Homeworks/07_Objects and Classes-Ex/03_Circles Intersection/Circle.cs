3e7e5176a3c5ea1ba29a82f75408d431db801a30
akkirilov <akkirilov@mail.bg>
2017-02-08 13:06:12 +0200

    Added 03_Circles Intersection

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/03_Circles Intersection/Circle.cs
@@ -0,0 +1,9 @@
+﻿namespace _03_Circles_Intersection
+{
+    class Circle
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Radius { get; set; }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/03_Circles Intersection/CirclesIntersection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Circles_Intersection
+{
+    class CirclesIntersection
+    {
+        static void Main()
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Circle firsCircle = new Circle { X = input[0], Y = input[1], Radius = input[2]};
+
+            input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Circle secondCircle = new Circle { X = input[0], Y = input[1], Radius = input[2] };
+
+            double distanceBetweenCenters = GetDistance(firsCircle, secondCircle);
+
+            if (distanceBetweenCenters <= (firsCircle.Radius + secondCircle.Radius))
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+            Console.WriteLine("No");
+        }
+
+        private static double GetDistance(Circle firsCircle, Circle secondCircle)
+        {
+            return Math.Sqrt(((Math.Pow((firsCircle.X - secondCircle.X), 2)) + (Math.Pow((firsCircle.Y - secondCircle.Y), 2))));
+        }
+    }
+}

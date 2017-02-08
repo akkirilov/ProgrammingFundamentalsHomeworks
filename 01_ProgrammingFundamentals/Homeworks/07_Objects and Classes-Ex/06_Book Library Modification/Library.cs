cda85b40bc8efb1e071e0bdbed2114dca733cefe
akkirilov <akkirilov@mail.bg>
2017-02-08 14:59:15 +0200

    Added 06_Book Library Modification

---
+++ 01_ProgrammingFundamentals/Homeworks/07_Objects and Classes-Ex/06_Book Library Modification/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Book_Library_Modification
+{
+    public class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books {get; set;}
+    }
+}

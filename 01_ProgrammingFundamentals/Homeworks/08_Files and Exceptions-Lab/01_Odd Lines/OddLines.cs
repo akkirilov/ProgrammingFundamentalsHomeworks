5a6e872d9d4d1c754fe580c5f72121424e712287
akkirilov <akkirilov@mail.bg>
2017-02-09 22:06:24 +0200

    Added 01_Odd Lines

---
+++ 01_ProgrammingFundamentals/Homeworks/08_Files and Exceptions-Lab/01_Odd Lines/OddLines.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace _01_Odd_Lines
+{
+    class OddLines
+    {
+        static void Main()
+        {
+            string[] textLines = File.ReadAllLines("input.txt");
+
+            List<string> onlyOddLines = new List<string>();
+
+            for (int i = 1; i < textLines.Length; i += 2)
+            {
+                onlyOddLines.Add(textLines[i]);
+            }
+
+            File.WriteAllLines("result.txt", onlyOddLines);
+        }
+    }
+}

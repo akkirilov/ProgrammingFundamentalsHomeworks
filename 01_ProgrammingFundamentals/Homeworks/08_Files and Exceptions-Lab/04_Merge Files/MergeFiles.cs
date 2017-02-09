b6ddc358db821d2c8f123540c497fae7bf3f0bc7
akkirilov <akkirilov@mail.bg>
2017-02-09 22:08:25 +0200

    Added 04_Merge Files AND 05_FolderSize

---
+++ 01_ProgrammingFundamentals/Homeworks/08_Files and Exceptions-Lab/04_Merge Files/MergeFiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace _04_Merge_Files
+{
+    class MergeFiles
+    {
+        static void Main()
+        {
+            DirectoryInfo thisDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            List<string> files = thisDir
+                .GetFiles()
+                .Select(x => x.Name)
+                .Where(x => x.Contains(".txt") && x != "result.txt")
+                .ToList();
+
+            List<string> mergeFiles = new List<string>();
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                mergeFiles.AddRange(File.ReadAllLines(files[i]).ToList());
+            }
+
+            File.WriteAllLines("result.txt", mergeFiles.OrderBy(x => x));
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/08_Files and Exceptions-Lab/05_FolderSize/FolderSize.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace _05_FolderSize
+{
+    class FolderSize
+    {
+        static void Main()
+        {
+            //! ! !  TestFolder need to be added in .\Debug  ! ! !
+
+            DirectoryInfo thisDir = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\TestFolder");
+
+            double size = thisDir.GetFiles().Select(x => x.Length / 1024.0 / 1024.0).Sum();
+
+            File.WriteAllText("result.txt", size.ToString());
+        }
+    }
+}

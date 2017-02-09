f1ba69a32d16fc510898b7959851e1a0eb6228f1
akkirilov <akkirilov@mail.bg>
2017-02-09 22:07:09 +0200

    Added 02_Line Numbers

---
+++ 01_ProgrammingFundamentals/Homeworks/08_Files and Exceptions-Lab/02_Line Numbers/LineNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace _02_Line_Numbers
+{
+    class LineNumbers
+    {
+        static void Main()
+        {
+            string[] textLines = File.ReadAllLines("input.txt");
+            List<string> numLines = new List<string>();
+
+            for (int i = 0; i < textLines.Length; i++)
+            {
+                numLines.Add($"{i + 1}. {textLines[i]}");
+            }
+
+            File.WriteAllLines("result.txt", numLines);
+        }
+    }
+}

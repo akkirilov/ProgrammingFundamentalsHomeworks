f92ea31f43b1a3e437ae4915988c6ac7f82b5be2
akkirilov <akkirilov@mail.bg>
2017-02-09 22:07:51 +0200

    Added 03_Word Count

---
+++ 01_ProgrammingFundamentals/Homeworks/08_Files and Exceptions-Lab/03_Word Count/WordCount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _03_Word_Count
+{
+    class WordCount
+    {
+        static void Main()
+        {
+            string[] textWords = File.ReadAllText("text.txt")
+                .ToLower()
+                .Split(new char[] { ' ', ',', '.', '!', '?', '&', '%', '#', '\r', '\n', '-', '_', '+' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] keyWords = File.ReadAllText("words.txt")
+                .ToLower()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> countWords = new Dictionary<string, int>();
+
+            for (int i = 0; i < keyWords.Length; i++)
+            {
+                countWords[keyWords[i]] = 0;
+
+                for (int j = 0; j < textWords.Length; j++)
+                {
+                    if (keyWords[i] == textWords[j])
+                    {
+                        countWords[keyWords[i]]++;
+                    }
+                }
+            }
+
+            File.WriteAllLines("result.txt", countWords.OrderByDescending(x => x.Value).Select(x => $"{x.Key} - {x.Value}"));
+        }
+    }
+}

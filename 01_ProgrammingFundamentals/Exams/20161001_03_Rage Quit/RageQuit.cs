b7579d0e95e7950a860b21614861e40c770df5b3
akkirilov <akkirilov@mail.bg>
2017-02-10 15:46:57 +0200

    need solution

---
+++ 01_ProgrammingFundamentals/Exams/20161001_03_Rage Quit/RageQuit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20161001_03_Rage_Quit
+{
+    class RageQuit
+    {
+        static void Main()
+        {
+
+
+
+        }
+    }
+}

7d549a910152da37d80619c14944fb91379ccf61
akkirilov <akkirilov@mail.bg>
2017-02-11 15:25:15 +0200

    format variables names

--- 01_ProgrammingFundamentals/Exams/20161001_03_Rage Quit/RageQuit.cs
+++ 01_ProgrammingFundamentals/Exams/20161001_03_Rage Quit/RageQuit.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace _20161001_03_Rage_Quit
 {
@@ -14,21 +13,21 @@ namespace _20161001_03_Rage_Quit
             string input = Console.ReadLine().ToUpper();
 
             string pattern = @"(.+?)(\d+)";
-            MatchCollection allSusStrings = new Regex(pattern).Matches(input);
+            MatchCollection allSubStrings = new Regex(pattern).Matches(input);
 
-            StringBuilder text = new StringBuilder();
-            foreach (Match subString in allSusStrings)
+            StringBuilder rageText = new StringBuilder();
+            foreach (Match subString in allSubStrings)
             {
                 for (int i = 0; i < int.Parse(subString.Groups[2].ToString()); i++)
                 {
-                    text.Append(subString.Groups[1].ToString()); 
+                    rageText.Append(subString.Groups[1].ToString()); 
                 }
             }
 
-            List<char> justToCount = text.ToString().Distinct().ToList();
+            List<char> uniqueSymbols = rageText.ToString().Distinct().ToList();
 
-            Console.WriteLine($"Unique symbols used: {justToCount.Count}");
-            Console.WriteLine(text.ToString());
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols.Count}");
+            Console.WriteLine(rageText.ToString());
         }
     }
 }

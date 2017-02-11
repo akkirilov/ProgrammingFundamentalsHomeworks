23ccfa55015cbd7c2377fa0748a64383725ef8bb
akkirilov <akkirilov@mail.bg>
2017-02-11 19:03:22 +0200

    Added 20161002_01_SweetDesert

---
+++ 01_ProgrammingFundamentals/Exams/20161002_01_SweetDesert/SweetDesert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _20161002_01_SweetDesert
+{
+    class SweetDesert
+    {
+        static void Main()
+        {
+            decimal totalMoney = decimal.Parse(Console.ReadLine());
+            int guests = int.Parse(Console.ReadLine());
+            decimal bananasPriceUnit = decimal.Parse(Console.ReadLine());
+            decimal eggsPriceUnit = decimal.Parse(Console.ReadLine());
+            decimal berriesKG = decimal.Parse(Console.ReadLine());
+
+            int portions = guests / 6 + 1;
+            if (guests % 6 == 0)
+            {
+                portions--;
+            }
+
+            decimal totalPrice = portions * ((bananasPriceUnit * 2) + (eggsPriceUnit * 4) + (berriesKG / 5));
+
+            if (totalMoney >= totalPrice)
+            {
+                Console.WriteLine("Ivancho has enough money - it would cost {0:f2}lv.", totalPrice);
+            }
+            else
+            {
+                Console.WriteLine("Ivancho will have to withdraw money - he will need {0:f2}lv more.", (totalPrice - totalMoney));
+            }
+        }
+    }
+}

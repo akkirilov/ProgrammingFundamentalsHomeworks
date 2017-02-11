5d72f5eb4b01ab4f57fbc901e90357b791d85d8a
akkirilov <akkirilov@mail.bg>
2017-02-11 15:18:25 +0200

    Added 20161001_03_Rage Quit (RegEx solution)

---
+++ 01_ProgrammingFundamentals/Exams/20161001_03_Rage Quit/RageQuit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20161001_03_Rage_Quit
+{
+    class RageQuit
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine().ToUpper();
+
+            string pattern = @"(.+?)(\d+)";
+            MatchCollection allSusStrings = new Regex(pattern).Matches(input);
+
+            StringBuilder text = new StringBuilder();
+            foreach (Match subString in allSusStrings)
+            {
+                for (int i = 0; i < int.Parse(subString.Groups[2].ToString()); i++)
+                {
+                    text.Append(subString.Groups[1].ToString()); 
+                }
+            }
+
+            List<char> justToCount = text.ToString().Distinct().ToList();
+
+            Console.WriteLine($"Unique symbols used: {justToCount.Count}");
+            Console.WriteLine(text.ToString());
+        }
+    }
+}

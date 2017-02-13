ea9027807d000bb877f278499089bec64fbbc6be
akkirilov <akkirilov@mail.bg>
2017-02-13 13:01:10 +0200

    Added 20161023_01_Charity Marathon

---
+++ 01_ProgrammingFundamentals/Exams/20161023_01_Charity Marathon/CharityMarathon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _20161023_01_Charity_Marathon
+{
+    class CharityMarathon
+    {
+        static void Main()
+        {
+            Console.WriteLine(int.MaxValue);
+            int days = int.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine());
+            int averageLaps = int.Parse(Console.ReadLine());
+            int trackLength = int.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKM = decimal.Parse(Console.ReadLine());
+
+            if (trackCapacity * days < runners)
+            {
+                runners = trackCapacity * days;
+            }
+
+            long totalKM = (runners * averageLaps * trackLength) / 1000;
+
+            decimal tottalMoney = totalKM * moneyPerKM;
+            Console.WriteLine($"Money raised: {tottalMoney:f2}");
+        }
+    }
+}

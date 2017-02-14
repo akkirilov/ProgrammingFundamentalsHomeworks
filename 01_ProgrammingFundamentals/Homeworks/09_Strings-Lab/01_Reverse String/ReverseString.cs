2740d1e2caa7f3b4ff45e896e52a2f33b3b0190e
akkirilov <akkirilov@mail.bg>
2017-02-14 10:05:00 +0200

    Added 01_Reverse String

---
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Lab/01_Reverse String/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Reverse_String
+{
+    class ReverseString
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string result = GetReversedString(input);
+            Console.WriteLine(result);
+        }
+
+        private static string GetReversedString(string input)
+        {
+            char[] temp = input.ToCharArray();
+            return (String.Join("", temp.Reverse()));
+        }
+    }
+}

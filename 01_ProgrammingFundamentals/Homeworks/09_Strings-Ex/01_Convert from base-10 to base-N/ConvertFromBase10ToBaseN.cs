1e6b192a017a29f5a047b4bdbdea6ff09fd71e27
akkirilov <akkirilov@mail.bg>
2017-02-15 10:16:28 +0200

    Added 01_Convert from base-10 to base-N

---
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/01_Convert from base-10 to base-N/ConvertFromBase10ToBaseN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace _01_Convert_from_base_10_to_base_N
+{
+    public class ConvertFromBase10ToBaseN
+    {
+        public static void Main()
+        {
+            string[] parameters = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            int toBase = int.Parse(parameters[0]);
+            BigInteger number = BigInteger.Parse(parameters[1]);
+
+            StringBuilder result = new StringBuilder();
+            while (number != 0)
+            {
+                result.Insert(0, number % toBase);
+                number /= toBase;
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}

0050a753c14618bcba2f4b1be755ad8163eaf70a
akkirilov <akkirilov@mail.bg>
2017-02-15 12:44:05 +0200

    Added 04_Character Multiplier

---
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/04_Character Multiplier/CharacterMultiplier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04_Character_Multiplier
+{
+    public class CharacterMultiplier
+    {
+        public static void Main()
+        {
+            string[] strings = Console.ReadLine().Split();
+
+            ulong result = 0;
+
+            for (int i = 0; i < Math.Min(strings[0].Length, strings[1].Length); i++)
+            {
+                result += (ulong)(strings[0][i] * strings[1][i]);
+            }
+
+            if (strings[0].Length > strings[1].Length)
+            {
+                for (int i = strings[1].Length; i < strings[0].Length; i++)
+                {
+                    result += (ulong)strings[0][i];
+                }
+            }
+            else if (strings[0].Length < strings[1].Length)
+            {
+                for (int i = strings[0].Length; i < strings[1].Length; i++)
+                {
+                    result += (ulong)strings[1][i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

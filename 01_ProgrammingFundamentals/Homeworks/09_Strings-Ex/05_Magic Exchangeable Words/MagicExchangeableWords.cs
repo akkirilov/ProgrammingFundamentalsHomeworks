cbda307bd62da066af12fa4ca79c68e3d427be62
akkirilov <akkirilov@mail.bg>
2017-02-15 13:01:33 +0200

    Added 05_Magic Exchangeable Words

---
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/05_Magic Exchangeable Words/MagicExchangeableWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Magic_Exchangeable_Words
+{
+    public class MagicExchangeableWords
+    {
+        public static void Main()
+        {
+            string[] strings = Console.ReadLine().Split();
+
+            int first = CountUniqueCharacters(strings[0]);
+            int second = CountUniqueCharacters(strings[1]);
+
+            if (first == second)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+        public static int CountUniqueCharacters(string s)
+        {
+            List<char> chars = s.ToCharArray()
+                .Distinct()
+                .ToList();
+
+            return chars.Count;
+        }
+    }
+}

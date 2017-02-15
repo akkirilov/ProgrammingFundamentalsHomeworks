e696960fe4ad292b3f4422ce313967050d0b79d3
akkirilov <akkirilov@mail.bg>
2017-02-15 12:33:33 +0200

    Added 03_Unicode Characters

---
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/03_Unicode Characters/UnicodeCharacters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03_Unicode_Characters
+{
+    public class UnicodeCharacters
+    {
+        public static void Main()
+        {
+            char[] textToChar = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < textToChar.Length; i++)
+            {
+                Console.Write("\\u{0:x4}",(int)textToChar[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}

0fdd20d2d733d7d140899c4180d7a295ecd76f89
akkirilov <akkirilov@mail.bg>
2017-02-18 00:01:22 +0200

    Added 02_Extract Sentences By Keyword

---
+++ 01_ProgrammingFundamentals/Homeworks/10_Regex-Ex/02_Extract Sentences By Keyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02_Extract_Sentences_By_Keyword
+{
+    public class ExtractSentencesByKeyword
+    {
+        public static void Main()
+        {
+            string key = Console.ReadLine();
+            string pattern = @"(\w[^.!?]*?\b" + key + @"\b[^.!?]*)";
+
+            MatchCollection sentences = new Regex(pattern).Matches(Console.ReadLine());
+            for (int i = 0; i < sentences.Count; i++)
+            {
+                Console.WriteLine(sentences[i]);
+            }
+        }
+    }
+}

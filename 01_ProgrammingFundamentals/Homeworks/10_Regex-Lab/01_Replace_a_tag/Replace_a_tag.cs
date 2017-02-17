8515edea4fcb1600120af26ed043d9a24fdaa446
akkirilov <akkirilov@mail.bg>
2017-02-17 16:47:45 +0200

    Added 10_Regex-Lab.sln with 01_Replace_a_tag

---
+++ 01_ProgrammingFundamentals/Homeworks/10_Regex-Lab/01_Replace_a_tag/Replace_a_tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_Replace_a_tag
+{
+    public class Replace_a_Tag
+    {
+        public static void Main()
+        {
+            string pattern = @"(.*)<a\s*(.+.\w+.)>(.+)<\/a>(.*)";
+
+            string input = Console.ReadLine();
+            while (!input.Equals("end"))
+            {
+                Console.WriteLine(new Regex(pattern).Replace(input, @"$1[URL $2]$3[/URL]$4"));
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}

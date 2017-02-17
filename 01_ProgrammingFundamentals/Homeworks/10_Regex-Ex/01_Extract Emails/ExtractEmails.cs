944b19362605557d6a105ca95a0c4f592a4c2342
akkirilov <akkirilov@mail.bg>
2017-02-17 23:09:36 +0200

    Added 10_Regex-Ex AND 01_Extract Emails

---
+++ 01_ProgrammingFundamentals/Homeworks/10_Regex-Ex/01_Extract Emails/ExtractEmails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_Extract_Emails
+{
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            string pattern = @"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))";
+
+            MatchCollection emails = new Regex(pattern).Matches(Console.ReadLine());
+            for (int i = 0; i < emails.Count; i++)
+            {
+                Console.WriteLine(emails[i]);
+            }
+        }
+    }
+}

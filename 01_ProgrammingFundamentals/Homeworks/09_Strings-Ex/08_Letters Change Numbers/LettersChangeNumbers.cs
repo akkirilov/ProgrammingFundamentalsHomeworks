eecf3bea1c3494ce4f4bbea3a1b86f74ef9fd6f0
akkirilov <akkirilov@mail.bg>
2017-02-17 11:12:48 +0200

    Added correct solution for 08_LettersChangeNumbers

--- 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/08_Letters Change Numbers/LettersChangeNumbers.cs
+++ 01_ProgrammingFundamentals/Homeworks/09_Strings-Ex/08_Letters Change Numbers/LettersChangeNumbers.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace _08_Letters_Change_Numbers
@@ -17,19 +14,33 @@ namespace _08_Letters_Change_Numbers
 
             for (int i = 0; i < matches.Count; i++)
             {
+                char firstChar = (matches[i].Groups[1]).ToString()[0];
+                decimal number = decimal.Parse(matches[i].Groups[2].ToString());
+                char secondChar = (matches[i].Groups[3]).ToString()[0];
                 decimal tempResult = 0;
-                char firstLetter = (matches[i].Groups[1]).ToString()[0];
-                Console.WriteLine("f: " + firstLetter);
-                Console.WriteLine("n: " + matches[i].Groups[2].ToString());
-                char secondLetter = (matches[i].Groups[3]).ToString()[0];
-                Console.WriteLine("s: " + secondLetter);
-                Console.WriteLine();
 
+                if (char.IsUpper(firstChar))
+                {
+                    tempResult = number / (int)(firstChar - 'A' + 1);
+                }
+                else if (char.IsLower(firstChar))
+                {
+                    tempResult = number * (int)(firstChar - 'a' + 1);
+                }
 
+                if (char.IsUpper(secondChar))
+                {
+                    tempResult -= (int)(secondChar - 'A' + 1);
+                }
+                else if (char.IsLower(secondChar))
+                {
+                    tempResult += (int)(secondChar - 'a' + 1);
+                }
+
+                result += tempResult;
             }
 
-
-
+            Console.WriteLine($"{result:f2}");
         }
     }
 }

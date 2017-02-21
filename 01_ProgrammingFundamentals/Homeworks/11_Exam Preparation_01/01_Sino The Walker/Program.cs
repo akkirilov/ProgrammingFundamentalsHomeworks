30432e7ce90069addb801665466a410650893774
akkirilov <akkirilov@mail.bg>
2017-02-21 20:33:52 +0200

    Added 01_Sino The Walker

---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_01/01_Sino The Walker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Sino_The_Walker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string timeFormat = @"HH:mm:ss";
+
+            DateTime time = DateTime.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+
+            long steps = long.Parse(Console.ReadLine()) % 86400;
+            long secondsPerStep = long.Parse(Console.ReadLine()) % 86400;
+
+            time = time.AddSeconds((ulong)(steps * secondsPerStep));
+
+            Console.WriteLine("Time Arrival: {0}", time.ToString(timeFormat));
+        }
+    }
+}

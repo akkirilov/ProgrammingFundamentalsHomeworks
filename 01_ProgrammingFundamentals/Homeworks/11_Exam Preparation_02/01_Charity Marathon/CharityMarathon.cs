a44e68d4c8bcd3422ddeca647325a9ea10492dab
akkirilov <akkirilov@mail.bg>
2017-02-21 19:18:34 +0200

    Added 11_Exam Preparation_02 with 01_Charity Marathon AND 02_Ladybugs

---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_02/01_Charity Marathon/CharityMarathon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Charity_Marathon
+{
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            int marathonLength = int.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine());
+            int averageLapsPerRunner = int.Parse(Console.ReadLine());
+            int trackLength = int.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKilometer = decimal.Parse(Console.ReadLine());
+
+            if (trackCapacity * marathonLength < runners)
+            {
+                runners = trackCapacity * marathonLength;
+            }
+
+            long kilometers = (runners * averageLapsPerRunner * trackLength) / 1000;
+            Console.WriteLine($"Money raised: {kilometers * moneyPerKilometer:f2}");
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_02/02_Ladybugs/LadyBugs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_Ladybugs
+{
+    public class LadyBugs
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] bugsIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] bugs = new int[length];
+            for (int i = 0; i < bugsIndexes.Length; i++)
+            {
+                if (bugsIndexes[i] >= 0 && bugsIndexes[i] < length)
+                {
+                    bugs[bugsIndexes[i]] = 1;
+                }
+            }
+
+            string[] input = Console.ReadLine().Split().ToArray();
+            while (!input[0].Equals("end"))
+            {
+                int bug = int.Parse(input[0]);
+                if (bug < 0 || bug >= length || bugs[bug] == 0)
+                {
+                    input = Console.ReadLine().Split().ToArray();
+                    continue;
+                }
+                
+                int flyLength = int.Parse(input[2]);
+                
+                if (input[1].Equals("left"))
+                {
+                    FlyLeft(bugs, bug, flyLength);
+                }
+                else if (input[1].Equals("right"))
+                {
+                    FlyRight(bugs, bug, flyLength);
+                }
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            Console.WriteLine(String.Join(" ", bugs));
+        }
+
+        public static void FlyLeft(int[] bugs, int bug, int flyLength)
+        {
+            if (flyLength < 0)
+            {
+                FlyRight(bugs, bug, Math.Abs(flyLength));
+                return;
+            }
+
+            bugs[bug] = 0;
+            while ((bug - flyLength) >= 0)
+            {
+                if (!bugs[bug - flyLength].Equals(1))
+                {
+                    bugs[bug - flyLength] = 1;
+                    return;
+                }
+                bug -= flyLength;
+            }
+        }
+
+        public static void FlyRight(int[] bugs, int bug, int flyLength)
+        {
+            if (flyLength < 0)
+            {
+                FlyLeft(bugs, bug, Math.Abs(flyLength));
+                return;
+            }
+
+            bugs[bug] = 0;
+            while ((bug + flyLength) < bugs.Length)
+            {
+                if (!bugs[bug + flyLength].Equals(1))
+                {
+                    bugs[bug + flyLength] = 1;
+                    return;
+                }
+                bug += flyLength;
+            }
+        }
+    }
+}

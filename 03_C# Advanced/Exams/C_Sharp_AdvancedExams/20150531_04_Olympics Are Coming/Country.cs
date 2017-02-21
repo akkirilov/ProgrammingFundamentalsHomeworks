880fcf63e18bf38cff8c3f10c5e3592638c68ca6
akkirilov <akkirilov@mail.bg>
2017-02-21 16:53:45 +0200

    Added 20150531_04_Olympics Are Coming

---
+++ 03_C# Advanced/Exams/C_Sharp_AdvancedExams/20150531_04_Olympics Are Coming/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _20150531_04_Olympics_Are_Coming
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public List<string> TotalAthletesWins { get; set; }
+
+        public int UniqueAthletes => TotalAthletesWins.Distinct().Count();
+    }
+}
---
+++ 03_C# Advanced/Exams/C_Sharp_AdvancedExams/20150531_04_Olympics Are Coming/OlympicsAreComing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _20150531_04_Olympics_Are_Coming
+{
+    public class OlympicsAreComing
+    {
+        public static void Main()
+        {
+            List<Country> countries = new List<Country>();
+
+            string input = Console.ReadLine();
+            while (!input.Equals("report"))
+            {
+                input = new Regex(@"\s{2,}").Replace(input, " ");
+                string[] inputEll = input
+                    .Split('|')
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                if (countries.Any(x => x.Name.Equals(inputEll[1])))
+                {
+                    int index = countries.FindIndex(x => x.Name.Equals(inputEll[1]));
+                    countries[index].TotalAthletesWins.Add(inputEll[0]);
+                }
+                else
+                {
+                    countries.Add(new Country
+                    {
+                        Name = inputEll[1],
+                        TotalAthletesWins = new List<string>(),
+                    });
+
+                    countries.Last().TotalAthletesWins.Add(inputEll[0]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var country in countries.OrderByDescending(x => x.TotalAthletesWins.Count))
+            {
+                Console.WriteLine($"{country.Name} ({country.UniqueAthletes} participants): {country.TotalAthletesWins.Count} wins");
+            }
+        }
+    }
+}

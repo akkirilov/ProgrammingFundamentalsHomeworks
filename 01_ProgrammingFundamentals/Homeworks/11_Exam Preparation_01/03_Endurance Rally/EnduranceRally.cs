4a8f9a61e60e9cbaa7b8cdb02225b712833a0a14
akkirilov <akkirilov@mail.bg>
2017-02-21 21:17:06 +0200

    Added 03_Endurance Rally

---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_01/03_Endurance Rally/EnduranceRally.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Endurance_Rally
+{
+    public class EnduranceRally
+    {
+        public static void Main()
+        {
+            List<string> drivers = Console.ReadLine().Split().Select(x => x.Trim()).ToList();
+
+            double[] trackPoints = Console.ReadLine().Split().Select(x => x.Trim()).Select(double.Parse).ToArray();
+            int[] checkPoints = Console.ReadLine().Split().Select(x => x.Trim()).Select(int.Parse).ToArray();
+
+            for (int d = 0; d < drivers.Count; d++)
+            {
+                double driverFuel = (int)drivers[d][0];
+                bool haveFuel = true;
+                for (int t = 0; t < trackPoints.Length; t++)
+                {
+                    if (checkPoints.Contains(t))
+                    {
+                        driverFuel += trackPoints[t];
+                    }
+                    else
+                    {
+                        driverFuel -= trackPoints[t];
+
+                        if (driverFuel <= 0)
+                        {
+                            Console.WriteLine($"{drivers[d]} - reached {t}");
+                            haveFuel = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (haveFuel)
+                {
+                    Console.WriteLine($"{drivers[d]} - fuel left {driverFuel:f2}");
+                }
+            }
+        }
+    }
+}

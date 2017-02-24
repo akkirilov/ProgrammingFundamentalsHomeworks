8e796c221cae4cb92649999de1d1bc982768fc7d
akkirilov <akkirilov@mail.bg>
2017-02-24 17:56:03 +0200

    03_Rage Quit AND 04_Files(Regex solution)

---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_03/03_Rage Quit/RageQuit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03_Rage_Quit
+{
+    public class RageQuit
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            MatchCollection matches = new Regex(@"([^\d]+)(\d+)").Matches(input);
+            StringBuilder rageText = new StringBuilder();
+
+            for (int m = 0; m < matches.Count; m++)
+            {
+                for (int i = 0; i < int.Parse(matches[m].Groups[2].ToString()); i++)
+                {
+                    rageText.Append(matches[m].Groups[1].ToString().ToUpper());
+                }
+            }
+
+            List<char> uniqueChars = rageText.ToString().ToCharArray().Distinct().ToList();
+            Console.WriteLine($"Unique symbols used: {uniqueChars.Count}");
+
+            Console.WriteLine(rageText.ToString());
+        }
+    }
+}
---
+++ 01_ProgrammingFundamentals/Homeworks/11_Exam Preparation_03/04_Files/Files.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04_Files
+{
+    class Files
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> inputs = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                inputs.Add(Console.ReadLine());
+            }
+
+            string[] parameters = Console.ReadLine().Split().Select(x => x.Trim()).ToArray();
+
+            Dictionary<string, ulong> files = new Dictionary<string, ulong>();
+            for (int i = 0; i < n; i++)
+            {
+                Match match = new Regex(@"^(\w+:*)\\*.*\\([^\\]+\.)(\w+);(\d+)$").Match(inputs[i]);
+
+                if (parameters[2].Equals(match.Groups[1].ToString()) && parameters[0].Equals(match.Groups[3].ToString()))
+                {
+                    files[(match.Groups[2].ToString() + match.Groups[3].ToString()).ToString()] = ulong.Parse(match.Groups[4].ToString());
+                }
+            }
+
+            if (files.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            foreach (var file in files.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{file.Key} - {file.Value} KB");
+            }
+        }
+    }
+}

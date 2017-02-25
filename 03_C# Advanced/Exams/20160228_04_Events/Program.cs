7335acb1e5b0fb99583f1e6eecd022d7c82bb9e7
akkirilov <akkirilov@mail.bg>
2017-02-25 15:37:35 +0200

    Added 20160228_04_Events (Regex solution)

---
+++ 03_C# Advanced/Exams/20160228_04_Events/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _20160228_04_Events
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SortedDictionary<string, Dictionary<string, List<DateTime>>> events = new SortedDictionary<string, Dictionary<string, List<DateTime>>>();
+
+            string pattern = @"^#([A-Za-z]+):\s*@([A-Za-z]+)\s*([0-9]{1,2})\:([0-9]{1,2})$";
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(input))
+                {
+                    Match match = regex.Match(input);
+                    string place = match.Groups[2].ToString();
+                    string person = match.Groups[1].ToString();
+                    int hour = int.Parse(match.Groups[3].ToString());
+                    int minutes = int.Parse(match.Groups[4].ToString());
+
+                    if (hour > 23 || minutes > 59)
+                    {
+                        continue;
+                    }
+
+                    DateTime currentTime = DateTime.Parse((hour + ":" + minutes).ToString());
+                    if (events.ContainsKey(place))
+                    {
+                        if (events[place].ContainsKey(person))
+                        {
+                            events[place][person].Add(currentTime);
+                        }
+                        else
+                        {
+                            events[place].Add(person, new List<DateTime>() { currentTime });
+                        }
+                    }
+                    else
+                    {
+                        events.Add(place, new Dictionary<string, List<DateTime>>() { [person] = new List<DateTime>() { currentTime } });
+                    }
+                }
+            }
+
+            string[] filter = Console.ReadLine().Split(',').Select(x => x.Trim()).ToArray();
+
+            foreach (var ev in events)
+            {
+                if (filter.Contains(ev.Key))
+                {
+                    int count = 1;
+                    Console.WriteLine($"{ev.Key}:");
+                    foreach (var person in ev.Value.OrderBy(x => x.Key))
+                    {
+                        List<string> times = person.Value.OrderBy(x => x).Select(x => x.ToString("HH:mm")).ToList();
+                        Console.WriteLine($"{count++}. {person.Key} -> {String.Join(", ", times)}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/Exams/20160228_04_Events/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("20160228_04_Events")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20160228_04_Events")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20747160-0e94-416c-acb0-6145b588832a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

f990d5a3d1038717fe94bb62fd799e5bc139fb42
akkirilov <akkirilov@mail.bg>
2017-03-24 10:21:21 +0200

    fixed calculator app

--- 02_SoftwareTechnologies/06_JS_ExpressJS_Calculator/controllers/home.js
+++ 02_SoftwareTechnologies/06_JS_ExpressJS_Calculator/controllers/home.js
@@ -1,5 +1,18 @@
+const Calculator = require('./../models/calculator');
+
 module.exports = {
     indexGet: (req, res) => {
         res.render('home/index');
+    },
+    indexPost: (req, res) => {
+        let calculatorParameters = req.body['calculator'];
+
+        let calculator = new Calculator;
+        calculator.leftOperand = Number(calculatorParameters.leftOperand);
+        calculator.rightOperand = Number(calculatorParameters.rightOperand);
+        calculator.operator = calculatorParameters.operator;
+        let result = calculator.calculate();
+
+        res.render('home/index', {'calculator':calculator, 'result':result});
     }
 };

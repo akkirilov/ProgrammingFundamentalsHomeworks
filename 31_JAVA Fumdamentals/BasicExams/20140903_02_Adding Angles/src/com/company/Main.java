babd4bee2d41745ac44147726cc28702b31c7dd2
akkirilov <akkirilov@mail.bg>
2017-04-09 23:35:26 +0300

    Added 20140903_02_Adding Angles

--- 31_JAVA Fumdamentals/BasicExams/20140903_02_Adding Angles/src/com/company/Main.java
+++ 31_JAVA Fumdamentals/BasicExams/20140903_02_Adding Angles/src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] tokens = new int[n];
+        for (int i = 0; i < n; i++) {
+            tokens[i] = scanner.nextInt();
+        }
+
+        boolean findSet = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if((tokens[i] + tokens[j] + tokens[k]) % 360 == 0){
+                        System.out.printf("%d + %d + %d = %d degrees%n",
+                                tokens[i],
+                                tokens[j],
+                                tokens[k],
+                                (tokens[i] + tokens[j] + tokens[k]));
+
+                        findSet = true;
+                    }
+                }
+            }
+        }
+
+        if (!findSet) {
+            System.out.println("No");
+        }
     }
 }

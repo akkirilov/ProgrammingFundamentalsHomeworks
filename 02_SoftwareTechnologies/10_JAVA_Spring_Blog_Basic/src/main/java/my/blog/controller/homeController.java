3615e08ff67c3c2c9b12beecb897379938aed8b7
akkirilov <akkirilov@mail.bg>
2017-04-10 11:14:50 +0300

    Finalize index view

--- 02_SoftwareTechnologies/10_JAVA_Spring_Blog_Basic/src/main/java/my/blog/controller/homeController.java
+++ 02_SoftwareTechnologies/10_JAVA_Spring_Blog_Basic/src/main/java/my/blog/controller/homeController.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class homeController {
@@ -17,6 +21,12 @@ public class homeController {
     @GetMapping("/")
     public String index(Model model){
         List<Article> articleList = articleRepository.findAll();
+        int skip = 6;
+        if (articleList.size() < 6) {
+            skip = articleList.size();
+        }
+        articleList = articleList.stream().skip(articleList.size() - skip).collect(Collectors.toList());
+        Collections.reverse(articleList);
 
         model.addAttribute("articles", articleList);
         model.addAttribute("view", "home/index");

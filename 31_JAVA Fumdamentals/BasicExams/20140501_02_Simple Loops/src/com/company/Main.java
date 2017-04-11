effccd479391870a65b6c12651db3c41467fccfe
akkirilov <akkirilov@mail.bg>
2017-04-11 17:55:03 +0300

    class added (Atom)

--- 31_JAVA Fumdamentals/BasicExams/20140501_02_Simple Loops/src/com/company/Main.java
+++ 31_JAVA Fumdamentals/BasicExams/20140501_02_Simple Loops/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
 	    BigInteger a = scanner.nextBigInteger();
         BigInteger b = scanner.nextBigInteger();
         BigInteger c = scanner.nextBigInteger();

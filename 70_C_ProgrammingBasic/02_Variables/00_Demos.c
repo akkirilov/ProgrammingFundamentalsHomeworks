bec685b6fcd610a71412b3fcfddd4de0b186cd20
akkirilov <akkirilov@mail.bg>
2017-04-13 01:37:08 +0300

    check variables

--- 70_C_ProgrammingBasic/02_Variables/00_Demos.c
+++ 70_C_ProgrammingBasic/02_Variables/00_Demos.c
@@ -6,14 +6,14 @@ int main() {
     long myLong = 64;
     float myFloat = 32;
     double myDouble = 64;
-    long double myLongDouble = 128.00 * 1.00;
+    long double myLongDouble = 128.00;
     char myChar  = '8';
 
     printf("Short = %d bits\n", myShort);
     printf("Int = %d bits\n", myInt);
     printf("Long = %ld bits\n", myLong);
-    printf("Float = %f bits\n", myFloat);
-    printf("Double = %lf bits\n", myDouble);
+    printf("Float = %.0f bits\n", myFloat);
+    printf("Double = %.0lf bits\n", myDouble);
     printf("Long double = %lf bits\n", myLongDouble);
     printf("Char = %c bits\n", myChar);
 
--- 70_C_ProgrammingBasic/02_Variables/02_BigNumbers.c
+++ 70_C_ProgrammingBasic/02_Variables/02_BigNumbers.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    long num1 = 20000000011L;
-    int num2 = 2000000001L;
+    long long int num1 = 1234567891045454545;
+    int num2 = 123456911;
     int num3 = 5656;
+    long double num4 = 5.3251515111515151545451;
 
-    printf("%ld\n%d\n%d\n", num1, num2, num3);
+    printf("long  : %lli\n", num1);
+    printf("int OF: %d\n", num2);
+    printf("int   : %d\n", num3);
+    printf("long D: %Lf\n", num4);
 
     return 0;
 }

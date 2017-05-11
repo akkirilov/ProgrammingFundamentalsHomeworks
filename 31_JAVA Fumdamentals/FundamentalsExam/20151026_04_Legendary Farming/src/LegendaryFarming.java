d022219cab719f7a6898d6a06c9bac63c592290a
akkirilov <akkirilov@mail.bg>
2017-05-11 16:17:28 +0300

    Added 20151026_04_Legendary Farming

--- 31_JAVA Fumdamentals/FundamentalsExam/20151026_04_Legendary Farming/src/LegendaryFarming.java
+++ 31_JAVA Fumdamentals/FundamentalsExam/20151026_04_Legendary Farming/src/LegendaryFarming.java
@@ -1,9 +1,83 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class LegendaryFarming {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Map<String, Integer> junks = new TreeMap<>();
+		
+		Map<String, Integer> materials = new HashMap<>();
+		materials.put("shards", 0);
+		materials.put("fragments", 0);
+		materials.put("motes", 0);
+		
+		int quantity;
+		String item = "";
+		
+		boolean enough = false;
+				
+		Scanner scanner = new Scanner(System.in);
+		String[] tokens = scanner.nextLine().split(" "); 
+		while (true) {
+			for (int i = 0; i < tokens.length; i += 2) {
+				quantity = Integer.parseInt(tokens[i]);
+				item = tokens[i + 1].toLowerCase();
+				
+				if (materials.containsKey(item)) {
+					materials.put(item, materials.get(item) + quantity);
+					
+					if (materials.get(item) >= 250){
+						materials.put(item, materials.get(item) - 250);
+						enough = true;
+						break;
+					}
+				}
+				else if (junks.containsKey(item)) {
+					junks.put(item, junks.get(item) + quantity);
+				}
+				else {
+					junks.put(item, quantity);
+				}
+			}
+			
+			if (enough) {
+				break;
+			}
+			
+			tokens = scanner.nextLine().toLowerCase().split("\\s+"); 
+		}
+			
+		if (item.equals("shards")) {
+			System.out.println("Shadowmourne obtained!");
+		}
+		else if (item.equals("motes")) {
+			System.out.println("Dragonwrath obtained!");
+		}
+		else {
+			System.out.println("Valanyr obtained!");
+		}
+		
+		List<Entry<String, Integer>> list = materials
+		        .entrySet()
+		        .stream()
+		        .sorted((Entry<String, Integer> o1, Entry<String, Integer> o2) -> {
+		             return o1.getValue().equals(o2.getValue()) ? 
+		            		 o1.getKey().compareTo(o2.getKey()) 
+		            		 : o2.getValue().compareTo(o1.getValue());
+		        	})
+		        .collect(Collectors.toList());
+		
+		for (Entry<String, Integer> entry : list) {
+			System.out.printf("%s: %d%n", entry.getKey(), entry.getValue());
+		}
+		
+		for (Map.Entry<String, Integer> entry : junks.entrySet()) {
+			System.out.printf("%s: %d%n", entry.getKey(), entry.getValue());
+		}
 	}
-
 }

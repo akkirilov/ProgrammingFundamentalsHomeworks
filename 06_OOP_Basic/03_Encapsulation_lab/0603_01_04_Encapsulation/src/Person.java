862c71870adf45d1da63044158f0a805f5d830e3
akkirilov <akkirilov@mail.bg>
2017-10-28 14:10:35 +0300

    add();

---
+++ 06_OOP_Basic/03_Encapsulation_lab/0603_01_04_Encapsulation/src/Person.java
@@ -0,0 +1,61 @@
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+	private double salary;
+	
+	public Person(String firstName, String lastName, int age, double salary) {
+		super();
+		setFirstName(firstName);
+		setLastName(lastName);
+		setAge(age);
+		setSalary(salary);
+	}
+	
+	private void setFirstName(String firstName) {
+		if (firstName.length() < 3) {
+			throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+		}
+		this.firstName = firstName;
+	}
+
+	private void setLastName(String lastName) {
+		if (lastName.length() < 3) {
+			throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+		}
+		this.lastName = lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	private void setAge(int age) {
+		if (age < 1) {
+			throw new IllegalArgumentException("Age cannot be zero or negative integer");
+		}
+		this.age = age;
+	}
+
+	private void setSalary(double salary) {
+		if (salary < 460) {
+			throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+		}
+		this.salary = salary;
+	}
+
+	public void increaseSalary (double percent) {
+		if (this.age <= 30) {
+			percent /= 2;
+		}
+		this.salary += (this.salary * percent) / 100;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s gets %s leva", firstName, lastName, salary);
+	}
+
+}

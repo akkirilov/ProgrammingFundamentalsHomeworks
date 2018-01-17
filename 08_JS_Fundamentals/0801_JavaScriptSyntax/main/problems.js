8658000006f8e202ee187ad998b660ca3b8a9986
akkirilov <akkirilov@mail.bg>
2018-01-17 20:32:52 +0200

    add()

--- 08_JS_Fundamentals/0801_JavaScriptSyntax/main/problems.js
+++ 08_JS_Fundamentals/0801_JavaScriptSyntax/main/problems.js
@@ -43,7 +43,45 @@ function filterByAge(minAge, nameA, ageA, nameB,ageB) {
 	}
 }
 
+//5. String of Numbers 1..N
+function stringOfNumbers(len) {
+	let str = "";
+	for (var i = 1; i <= len; i++) {
+		str += i;
+	}
+	console.log(str);
+}
+
+//6. Figure Area
+function figureArea(w, h, W, H) {
+	let [s1, s2, s3] = [
+		w * h, 
+		W * H, 
+		((w * h) +(W * H)) - (Math.min(H, h) * Math.min(W, w))
+	];
+	console.log(s3);
+}
+
+//7. Next Day
+function nextDay(year, month, day) {
+	let oneDay = 60 * 60 * 24 * 1000;
+	let currDate = new Date(year, month - 1, day);
+	let nextDate = new Date(currDate.getTime() + oneDay);
+	let res = nextDate.getFullYear() + "-" + (nextDate.getMonth() + 1) + "-" + nextDate.getDate();
+	console.log(res);
+}
+
+//8. Distance between Points
+function distanceBetweenPoints(x1, y1, x2, y2) {
+	let distance = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	console.log(distance);
+}
+
 //sumThreeNumbers(1, 2, 3);
 //sumAndVAT([1.20, 2.60, 3.50]);
 //letterOccurencesInString("peshoto", "o");
-filterByAge(12, 'Ivan', 15, 'Asen', 9);
+//filterByAge(12, 'Ivan', 15, 'Asen', 9);
+//stringOfNumbers(11);
+//figureArea(2, 4, 5, 3);
+//nextDay(2016, 2, 28);
+//distanceBetweenPoints(2, 4, 5, 0);

6647f2e533a8c521812a1bcf7328ed8806b64742
akkirilov <akkirilov@mail.bg>
2018-01-23 23:26:39 +0200

    add(05_Functions_ex)

--- 08_JS_Fundamentals/JavaScript/05_Functions_ex/problems.js
+++ 08_JS_Fundamentals/JavaScript/05_Functions_ex/problems.js
@@ -114,6 +114,76 @@ function modifyAverage(num) {
 }
 
 // 06. Validity Checker
+function validityChecker(arr) {
+	let xA = arr[0]; 
+	let yA = arr[1];
+	let xB = arr[2];
+	let yB = arr[3];
+	
+	console.log(isValid(xA, yA, 0, 0));
+	console.log(isValid(xB, yB, 0, 0));
+	console.log(isValid(xA, yA, xB, yB));
+	
+	function isValid(xA, yA, xB, yB) {
+		let res = Math.sqrt(Math.pow((xA - xB), 2) + Math.pow((yA - yB), 2));
+		let msg = `{${xA}, ${yA}} to {${xB}, ${yB}} is `;
+		if (res === Math.floor(res)) {
+			msg +='valid';
+		} else {
+			msg += 'invalid';
+		}
+		return msg;
+	}
+}
+
+//07. Treasure Locator
+function treasureLocator(arr) {
+	let tuvaluXa = 1;
+	let tuvaluYa = 3;
+	let tuvaluXb = 3;
+	let tuvaluYb = 1;
+	let tongaXa = 0;
+	let tongaYa = 8;
+	let tongaXb = 2;
+	let tongaYb = 6;
+	let samoaXa = 5;
+	let samoaYa = 6;
+	let samoaXb = 7;
+	let samoaYb = 3;
+	let tokelauXa = 8;
+	let tokelauYa = 1;
+	let tokelauXb = 9;
+	let tokelauYb = 0;
+	let cookXa = 4;
+	let cookYa = 8;
+	let cookXb = 9;
+	let cookYb = 7;
+	
+	for (var i = 0; i < arr.length - 1; i+=2) {
+		checkPoint(arr[i], arr[i + 1]);
+	}
+	
+	function checkPoint(x, y) {
+		if (x >= tuvaluXa && x <= tuvaluXb && y <= tuvaluYa && y >= tuvaluYb) {
+			console.log("Tuvalu");
+		} else if (x >= tongaXa && x <= tongaXb && y <= tongaYa && y >= tongaYb) {
+			console.log("Tonga");
+		} else if (x >= samoaXa && x <= samoaXb && y <= samoaYa && y >= samoaYb) {
+			console.log("Samoa");
+		} else if (x >= tokelauXa && x <= tokelauXb && y <= tokelauYa && y >= tokelauYb) {
+			console.log("Tokelau");
+		} else if (x >= cookXa && x <= cookXb && y <= cookYa && y >= cookYb) {
+			console.log("Cook");
+		} else {
+			console.log("On the bottom of the ocean");
+		}
+	}
+}
+
+// 08. Trip Length
+function tripLength() {
+	
+}
 
 
 //insideTheBox([13.1, 50, 31.5, 50, 80, 50, -5, 18, 43]);
@@ -121,5 +191,5 @@ function modifyAverage(num) {
 //templateFormat([ 'a', 'b' ]);
 //cookingByNumbers([9, 'dice', 'spice', 'chop', 'bake', 'fillet']);
 //modifyAverage('101');
-
-
+//validityChecker([3, 0, 0, 4]);
+treasureLocator([4, 2, 1.5, 6.5, 1, 3]);

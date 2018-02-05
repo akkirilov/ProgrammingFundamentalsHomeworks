08924c3ae6b6e11f5a199603ff706443876af38b
akkirilov <akkirilov@mail.bg>
2018-02-05 14:39:56 +0200

    update(20140729_exam);

--- 08_JS_Fundamentals/JavaScript/20_BasicExams/20140729_exam.js
+++ 08_JS_Fundamentals/JavaScript/20_BasicExams/20140729_exam.js
@@ -179,3 +179,62 @@ function biggestTableRow(arr) {
     	console.log(maxSum + ' = ' + lines[maxSum].join(' + '));
     }
 }
+
+// 04. Students, Courses, Grades
+//students(['Peter Nikolov | PHP  | 5.50 | 8',
+//	'Maria Ivanova | Java | 5.83 | 7',
+//	'Ivan Petrov   | PHP  | 3.00 | 2',
+//	'Ivan Petrov   | C#   | 3.00 | 2',
+//	'Peter Nikolov | C#   | 5.50 | 8',
+//	'Maria Ivanova | C#   | 5.83 | 7',
+//	'Ivan Petrov   | C#   | 4.12 | 5',
+//	'Ivan Petrov   | PHP  | 3.10 | 2',
+//	'Peter Nikolov | Java | 6.00 | 9']);
+students(['Milen Georgiev|PHP|2.02|2',
+	'Ivan Petrov | C# Basics | 3.20 | 22',
+	'Peter Nikolov | C# | 5.522 | 18',
+	'Maria Kirova | C# Basics | 5.40 | 4.5',
+	'Stanislav Peev | C# | 4.012 | 15',
+	'Ivan Petrov |    PHP Basics     |     5.120 |6',
+	'Ivan Goranov | SQL | 5.926 | 12',
+	'Todor Kirov | Databases | 5.50 |0.0000',
+	'Maria Ivanova | Java | 5.83 | 37',
+	'Milena Ivanova |    C# |   1.823 | 4.5',
+	'Stanislav Peev   |    C#|   4.122    |    15   ',
+	'Kiril Petrov |PHP| 5.10 | 6',
+	'Ivan Petrov|SQL|5.926|3',
+	'Peter Nikolov   |    Java  |   5.9996    |    9   ',
+	'Stefan Nikolov | Java | 4.00 | 9.5',
+	'Ivan Goranov | SQL | 5.926 | 12.5',
+	'Todor Kirov | Databases | 5.150 |0.0000',
+	'Kiril Ivanov | Java | 5.083 | 327',
+	'Diana Ivanova |    C# |   1.823 | 4',
+	'Stanislav Peev   |    C#|   4.122    |    15   ',
+	'Kiril Petrov |PHP| 5.10 | 6']);
+function students(arr) {
+	let res = {};
+	for (let e of arr) {
+		let [name, course, grade, visits] = e.split(/\s*\|\s*/).filter(x => x !== '');
+		if (res[course]) {
+			res[course]['avgGrade'].push(Number(grade));
+			res[course]['avgVisits'].push(Number(visits));
+			if (res[course]['students'].indexOf(name) < 0) {
+				res[course]['students'].push(name);
+			}
+		} else {
+			res[course] = {};
+			res[course]['avgGrade'] = [Number(grade)];
+			res[course]['avgVisits'] = [Number(visits)];
+			res[course]['students'] = [name];
+		}
+	}
+	let sortedCourses = {};
+	let coursesKeys = Object.keys(res).sort();
+	for (let ck of coursesKeys) {
+		sortedCourses[ck] = {};
+		sortedCourses[ck]['avgGrade'] = Number((res[ck]['avgGrade'].reduce((a, b) => a + b) / res[ck]['avgGrade'].length).toFixed(2).toString());
+		sortedCourses[ck]['avgVisits'] = Number((res[ck]['avgVisits'].reduce((a, b) => a + b) / res[ck]['avgVisits'].length).toFixed(2).toString());
+		sortedCourses[ck]['students'] = res[ck]['students'].sort();
+	}
+	console.log(JSON.stringify(sortedCourses).replace(',"undefined":{"avgGrade":null,"avgVisits":null,"students":[null]}', '')+'\n');
+}

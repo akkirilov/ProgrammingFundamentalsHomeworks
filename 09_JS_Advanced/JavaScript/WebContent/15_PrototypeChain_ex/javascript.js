4c86be5a65452bbdf65e23c1cb46659afad18daa
akkirilov <akkirilov@mail.bg>
2018-03-11 11:48:32 +0200

    add(15_PrototypeChain_ex, "1-7");

--- 09_JS_Advanced/JavaScript/WebContent/15_PrototypeChain_ex/javascript.js
+++ 09_JS_Advanced/JavaScript/WebContent/15_PrototypeChain_ex/javascript.js
@@ -300,6 +300,123 @@ class Console {
 	}
 };
 module.exports = { Console };
-let c = new Console();
-console.log(Console.writeLine('saas'));
+
+// 06. Computer
+function computer() {
+	class Keyboard  {
+		constructor(manufacturer, responseTime) {
+			this.manufacturer = manufacturer;
+			this.responseTime = responseTime;
+		}
+	}
+	class Monitor {
+		constructor(manufacturer, width, height) {
+			this.manufacturer = manufacturer;
+			this.width = width;
+			this.height = height;
+		}
+	}
+	class Battery  {
+		constructor(manufacturer, expectedLife) {
+			this.manufacturer = manufacturer;
+			this.expectedLife = expectedLife;
+		}
+	}
+	class Computer {
+		constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+			if (this.constructor.name === "Computer") {
+				throw new Error('Abstract class cannot be instantiated directly');
+			}
+			this.manufacturer = manufacturer;
+			this.processorSpeed = processorSpeed;
+			this.ram = ram;
+			this.hardDiskSpace = hardDiskSpace;
+		}
+	}
+	class Laptop extends Computer {
+		constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery) {
+			super(manufacturer, processorSpeed, ram, hardDiskSpace);
+			this.weight = weight;
+			this.color = color;
+			this.battery = battery;
+		}
+		get battery() {
+			return this._battery;
+		}
+		set battery(battery) {
+			if (!(battery instanceof Battery)) {
+				throw new TypeError('battery not instance of Battery class');
+			}
+			this._battery = battery;
+		}
+	}
+	class Desktop extends Computer {
+		constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor) {
+			super(manufacturer, processorSpeed, ram, hardDiskSpace);
+			this.keyboard = keyboard;
+			this.monitor = monitor;
+		}
+		get keyboard() {
+			return this._keyboard;
+		}
+		set keyboard(keyboard) {
+			if (!(keyboard instanceof Keyboard)) {
+				throw new TypeError('keyboard not instance of Keyboard class');
+			}
+			this._keyboard = keyboard;
+		}
+		get monitor() {
+			return this._monitor;
+		}
+		set monitor(monitor) {
+			if (!(monitor instanceof Monitor)) {
+				throw new TypeError('monitor not instance of Monitor class');
+			}
+			this._monitor = monitor;
+		}
+	}
+	return { Keyboard, Monitor, Battery, Computer, Laptop, Desktop };
+}
+
+// 07. Mixins
+function createMixins() {
+	function computerQualityMixin(clazz) {
+		clazz.prototype.getQuality = function() {
+			return ((this.processorSpeed + this.ram + this.hardDiskSpace) / 3);
+		}
+		clazz.prototype.isFast = function() {
+			if (this.processorSpeed > (this.ram / 4)) {
+				return true
+			}
+			return false;
+		}
+		clazz.prototype.isRoomy = function() {
+			if (this.hardDiskSpace > Math.floor(this.ram * this.processorSpeed)) {
+				return true
+			}
+			return false;
+		}
+	}
+	function styleMixin(clazz) {
+		clazz.prototype.isFullSet = function() {
+			if (this.manufacturer === this.keyboard.manufacturer
+				&& this.manufacturer === this.monitor.manufacturer) {
+				return true
+			}
+			return false;
+		}
+		clazz.prototype.isClassy = function() {
+			if (this.battery.expectedLife >= 3 
+				&& (this.color === 'Silver' || this.color === 'Black')
+				&& this.weight < 3) {
+				return true
+			}
+			return false;
+		}
+	}
+    return {
+        computerQualityMixin,
+        styleMixin
+    }
+}
 
--- 09_JS_Advanced/JavaScript/test/p15_UnitTests.js
+++ 09_JS_Advanced/JavaScript/test/p15_UnitTests.js
@@ -9,11 +9,20 @@ describe('Console tests', function() {
 	it('writeLine("just {0} string", "a") => just a string', function() {
 		expect(Console.writeLine("just {0} string", "a")).to.be.equal('just a string');
 	})
-	it('writeLine("just {0} {1}", "a", "string") => just a string', function() {
-		expect(Console.writeLine("just {0} {1}", "a", "string")).to.be.equal('just a string');
+	it('writeLine("just {0} {1}{2}", "a", "string", "!") => just a string', function() {
+		expect(Console.writeLine("just {0} {1}{2}", "a", "string", "!")).to.be.equal('just a string!');
 	})
-	it('writeLine({prop: "prop", prop1: 2}) => {"prop":"prop","prop1":2}', function() {
-		expect(Console.writeLine({prop: "prop", prop1: 2})).to.be.equal('{"prop":"prop","prop1":2}');
+	it('writeLine() => {"prop":"prop","prop1":2}', function() {
+		expect(Console.writeLine({prop: "prop", prop1: 2})).to.be.equal(JSON.stringify({prop: "prop", prop1: 2}));
+	})
+	it('writeLine([]) => []', function() {
+		expect(Console.writeLine([])).to.be.equal('[]');
+	})
+	it('writeLine(1) => undefined', function() {
+		expect(Console.writeLine(1)).to.be.equal(undefined);
+	})
+	it('writeLine(1.5) => undefined', function() {
+		expect(Console.writeLine(1.5)).to.be.equal(undefined);
 	})
 	it('writeLine("just {0} {1}", "a") => RangeError', function() {
 		assert.throws(function () { Console.writeLine("just {0} {1}", "a") }, RangeError, "Incorrect amount of parameters given!");
@@ -21,8 +30,16 @@ describe('Console tests', function() {
 	it('writeLine("just {0} {2}", "a", "string") => RangeError', function() {
 		assert.throws(function () { Console.writeLine("just {0} {2}", "a", "string") }, RangeError, "Incorrect placeholders given!");
 	})
+	it('writeLine("just {0} ", "a", "string") => RangeError', function() {
+		assert.throws(function () { Console.writeLine("just {0} ", "a", "string") }, RangeError, "Incorrect amount of parameters given!");
+	})
+	it('writeLine("just {15} ", "a", "string", "!") => RangeError', function() {
+		assert.throws(function () { Console.writeLine("just {15} ", "a", "string", "!") }, RangeError, "Incorrect amount of parameters given!");
+	})
 	it('writeLine(1, "a", "string") => TypeError', function() {
 		assert.throws(function () { Console.writeLine(1, "a", "string") }, TypeError, "No string format given!");
 	})
-	
+	it('writeLine() => TypeError', function() {
+		assert.throws(function () { Console.writeLine() }, TypeError, "No string format given!");
+	})
 })

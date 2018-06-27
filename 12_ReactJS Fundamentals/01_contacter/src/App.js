1a1a0cdb6c72b7d118cd022a3a64c00a0bbd54a7
akkirilov <akkirilov@mail.bg>
2018-06-27 17:19:33 +0300

    finalize();

--- 12_ReactJS Fundamentals/01_contacter/src/App.js
+++ 12_ReactJS Fundamentals/01_contacter/src/App.js
@@ -1,83 +1,106 @@
-  import React, { Component } from 'react';
-  import logo from './logo.svg';
-  import './App.css';
+import React, { Component } from 'react'; 
+import './App.css';
 
-  let contactsData = require('./contacts.json').map(x => {
-        <Contact firstName={x.firstName} lastName={x.lastName} />
-      }).join(''); 
-console.log(contactsData)
+let contactsData = require('./contacts.json');
+let currentId = 0;
 
-  class Header extends Component {
+class Header extends Component {
     render() {
-      return (
-    <header>&#9993; Contact Book</header>
-      );
+        return (
+            <header>&#9993; Contact Book</header>
+        );
     }
-  }
+}
 
-  class Contact extends Component {
+class Footer extends Component {
     render() {
-      return (
-        <div className="contact" >
+        return (
+            <footer>Contact Book SPA &copy; 2017</footer>
+        );
+    }
+}
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="contact" key={this.props.index} data-id={this.props.index} onClick={this.props.click} >
             <span className="avatar small">&#9787;</span>
             <span className="title">{this.props.firstName} {this.props.lastName}</span>
-        </div>
-      );
+            </div>
+        );
     }
-  }
+}
 
-  class Content extends Component {
-      constructor(props) {
-        super(props);
-        this.contactsData = require('./contacts.json').map(x => {
-          <Contact firstName={x.firstName} lastName={x.lastName} />
-         });
-      }
-       
+class ContactDetails extends Component {
     render() {
-      return (
-        <div>
-        {contactsData}
-        </div>
-      );
+        return (
+            <div className="content">
+                <div className="info">
+                    <div className="col">
+                    <span className="avatar">&#9787;</span>
+                    </div>
+                    <div className="col">
+                    <span className="name">{this.props.person.firstName}</span>
+                    <span className="name">{this.props.person.lastName}</span>
+                    </div>
+                </div>
+                <div className="info">
+                    <span className="info-line">&#9742; {this.props.person.phone}</span>
+                    <span className="info-line">&#9993; {this.props.person.email}</span>
+                </div>
+            </div> 
+        ); 
     }
-  }
+}
 
-  class App extends Component {
-      render() {
-      return (
-        <div>
-        <Header />
-        <div id="book">
-          <div id="list">
-              <h1>Contacts</h1>
-              
-              <Content>
-                
-              </Content>
-          </div>
-          <div id="details">
-              <h1>Details</h1>
-              <div className="content">
-                  <div className="info">
-                      <div className="col">
-                          <span className="avatar">&#9787;</span>
-                      </div>
-                      <div className="col">
-                          <span className="name">Ivan</span>
-                          <span className="name">Ivanov</span>
-                      </div>
-                  </div>
-                  <div className="info">
-                      <span className="info-line">&phone; 0887 123 456</span>
-                      <span className="info-line">&#9993; i.ivanov@gmail.com</span>
-                  </div>
-              </div>
-          </div>
-      </div>
-      </div>
-      );
+class Content extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: contactsData.map((x, i) => {
+                return <Contact click={this.handleClick.bind(this)} firstName={x.firstName} lastName={x.lastName} key={i} index={i}/>
+            }),
+            details: <ContactDetails person={contactsData[currentId]}/>
+        }
     }
-  }
+    handleClick(event) {
+        let id = event.target.parentNode.getAttribute('data-id');
+        if (id == null) {
+            id = event.target.getAttribute('data-id');
+        }
+        currentId = Number(id);
+        this.setState(state => ({
+            details: <ContactDetails person={contactsData[currentId]}/>
+        }));
+    }
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <div id="book">
+                    <div id="list">
+                        <h1>Contacts</h1>
+                        <div className="content">
+                            {this.state.contacts}
+                        </div>
+                    </div>
+                    <div id="details">
+                        <h1>Details</h1>
+                        {this.state.details}
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
 
-  export default App;
+class App extends Component {
+    render() {
+        return (
+            <Content />
+        );
+    }
+}
+
+export default App;
